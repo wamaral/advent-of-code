902cc60463a7052d82245af61a821041c53615e8
Wagner Amaral <wamaral@bionexo.com>
2015-12-03 19:00:42 -0200

    Solves 2.2

--- src/advent_of_code/day2.clj
+++ src/advent_of_code/day2.clj
@@ -11,5 +11,15 @@
        (* 2 z)
        (min x y z))))
 
+(defn ribbon [acc box]
+  (let [[l w h] (map read-string (re-seq #"\d+" box))
+        bow (* l w h)
+        mins (take 2 (sort [l w h]))
+        wrap (reduce + (map #(* 2 %) mins))]
+    (+ acc bow wrap)))
+
 (with-open [rdr (clojure.java.io/reader "resources/day2.input")]
-  (reduce sum-area 0 (line-seq rdr)))
+  (println (str "Total paper: " (reduce sum-area 0 (line-seq rdr)))))
+
+(with-open [rdr (clojure.java.io/reader "resources/day2.input")]
+  (println (str "Total ribbon: " (reduce ribbon 0 (line-seq rdr)))))
