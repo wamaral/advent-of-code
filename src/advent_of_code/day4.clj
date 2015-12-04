1acc5a3049426d856a710241e09b9f69c08a3efc
Wagner Amaral <wamaral@bionexo.com>
2015-12-04 19:32:46 -0200

    Solves 4.2 (slow)

--- src/advent_of_code/day4.clj
+++ src/advent_of_code/day4.clj
@@ -1,14 +1,18 @@
 (ns advent-of-code.day4
   (:require digest))
 
-(defn five-zeroes? [md5]
-  (= "00000"
-     (subs md5 0 5)))
+(defn n-zeroes? [n md5]
+  (= (repeat n \0)
+     (take n md5)))
 
-(defn get-hashes
-  ([input] (get-hashes input 1))
+(defn hashes
+  ([input] (hashes input 1))
   ([input n] (cons (digest/md5 (str input n))
-                   (lazy-seq (get-hashes input (inc n))))))
+                   (lazy-seq (hashes input (inc n))))))
+
+(defn result [input n]
+  (inc (count (take-while (complement (partial n-zeroes? n))
+                          (hashes input)))))
 
 (let [input "yzbqklnj"]
-  (inc (count (take-while (complement five-zeroes?) (get-hashes input)))))
+  (pmap (partial result input) [5 6]))
