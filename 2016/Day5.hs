0394a9ca40d19b260d83b694af0cb1e94218d34c
Wagner Amaral <wamaral@bionexo.com>
2016-12-05 13:37:57 -0200

    5.2 solution

--- 2016/Day5.hs
+++ 2016/Day5.hs
@@ -11,11 +11,17 @@ hash = show . md5 . pack
 isKey :: String -> Bool
 isKey = isPrefixOf "00000" . hash
 
+keyForPos :: Integer -> String -> Bool
+keyForPos i = isPrefixOf ("00000" ++ (show i)) . hash
+
+relevantHashes :: String -> [String]
+relevantHashes input = filter isKey $ map ((input ++) . show) [0..]
+
 -- Main
 main :: IO ()
 main = do
   input <- readFile "input/5"
   putStr "1. "
-  putStrLn $ show $ map ((!! 5) . hash) $ take 8 $ filter isKey $ map ((input ++) . show) [0..]
+  putStrLn $ show $ map ((!! 5) . hash) $ take 8 $ relevantHashes input
   putStr "2. "
-  putStrLn "TODO"
+  putStrLn $ show $ map (((!! 6) . hash) . (\i -> head $ filter (keyForPos i) $ relevantHashes input)) [0..7]
