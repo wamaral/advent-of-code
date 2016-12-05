513b69b979c1afe8485f3c02c7f04ceaa43f10fb
Wagner Amaral <wamaral@bionexo.com>
2016-12-04 23:59:32 -0200

    3.2 solution

--- 2016/Day3.hs
+++ 2016/Day3.hs
@@ -1,13 +1,17 @@
 module Day3 where
 
-import           Data.List (all, permutations)
+import           Data.List       (all, permutations, transpose)
+import           Data.List.Split (chunksOf)
 
+-- Types
 data Triangle = Triangle Integer Integer Integer
   deriving Show
 
+-- Logic
 validTriangle :: Triangle -> Bool
 validTriangle (Triangle a b c) = all (\(x:y:z:_) -> x + y > z) $ permutations [a, b, c]
 
+-- Parse
 makeTriangle :: String -> Triangle
 makeTriangle line = Triangle a b c
   where
@@ -18,7 +22,12 @@ makeTriangle line = Triangle a b c
 main :: IO ()
 main = do
   inputs <- readFile "input/3"
+  let fullData = concatMap words $ lines inputs
+  let input1 = chunksOf 3 fullData
+  let input2 = chunksOf 3 $ concat $ transpose input1
   putStr "1. "
-  putStrLn $ show $ length $ filter validTriangle $ map makeTriangle $ lines inputs
+  putStrLn $ show $ length $ validTriangles input1
   putStr "2. "
-  putStrLn "TODO"
+  putStrLn $ show $ length $ validTriangles input2
+  where
+    validTriangles input = filter validTriangle $ map (makeTriangle . unwords) input
