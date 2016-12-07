c7f49ea34382c20c8a27efdc34ba9902f3c800e3
Wagner Amaral <wamaral@bionexo.com>
2016-12-07 14:55:11 -0200

    7.2 solution

--- 2016/Day7.hs
+++ 2016/Day7.hs
@@ -1,5 +1,6 @@
 module Day7 where
 
+import           Data.List       (isInfixOf)
 import           Data.List.Split (divvy, split, startsWithOneOf)
 
 -- Types
@@ -14,6 +15,12 @@ abba = any (\(x:y:z:w:_) -> x == w && y == z && x /= y) . divvy 4 1
 tls :: Ipv7 -> Bool
 tls i = (any abba $ outsides i) && (all (not . abba) $ insides i)
 
+ssl :: Ipv7 -> Bool
+ssl i = any bab aba
+  where
+    aba = filter (\(x:y:z:_) -> x == z && x /= y) $ concatMap (divvy 3 1) (outsides i)
+    bab (r:s:_) = any (\x -> [s, r, s] `isInfixOf` x) (insides i)
+
 -- Parse
 makeIpv7 :: String -> Ipv7
 makeIpv7 input = foldl append Ipv7 { insides = [], outsides = [] } $ split (startsWithOneOf "[]") input
@@ -26,7 +33,8 @@ makeIpv7 input = foldl append Ipv7 { insides = [], outsides = [] } $ split (star
 main :: IO ()
 main = do
   input <- lines <$> readFile "input/7"
+  let inputs = map makeIpv7 input
   putStr "1. "
-  putStrLn $ show $ length $ filter tls $ map makeIpv7 input
+  putStrLn $ show $ length $ filter tls inputs
   putStr "2. "
-  putStrLn "TODO"
+  putStrLn $ show $ length $ filter ssl inputs
