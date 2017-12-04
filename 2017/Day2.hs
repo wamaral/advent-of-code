464301366cdeee7320e3c1b7dd5413a16c75f177
Wagner Amaral <wagner.amaral@nubank.com.br>
2017-12-04 12:57:12 -0200

    2017/2.2

--- 2017/Day2.hs
+++ 2017/Day2.hs
@@ -9,10 +9,16 @@ readAsInteger x = read x :: Integer
 firstAndLast :: [a] -> (a, a)
 firstAndLast xs = (head xs, last xs)
 
+cartesian :: [a] -> [(a,a)]
+cartesian xs = (,) <$> xs <*> xs
+
+evenlyDivides :: (Integer, Integer) -> Bool
+evenlyDivides (x,y) = x /= y && x `mod` y == 0
+
 main :: IO ()
 main = do
   input <- map (map readAsInteger . words) . lines <$> readFile "input/2"
   putStr "1. "
   print $ sum $ map (uncurry (flip (-)) . firstAndLast . sort) input
-  -- putStr "2. "
-  -- print ""
+  putStr "2. "
+  print $ sum $ map (head . map (uncurry div) . filter evenlyDivides . cartesian) input
