85991599a3e275e32bde66a07cc1591a73813f4b
Wagner Amaral <wagner.amaral@nubank.com.br>
2017-12-06 13:34:40 -0200

    2017/4.1

---
+++ 2017/Day4.hs
@@ -0,0 +1,14 @@
+module Day4 where
+
+import           Data.Set (fromList, size)
+
+nonDuplicate :: Ord a => [a] -> Bool
+nonDuplicate xs = length xs == size (fromList xs)
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "input/4"
+  putStr "1. "
+  print $ length $ filter (nonDuplicate . words) input
+  -- putStr "2. "
+  -- print ""
