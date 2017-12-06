40c4ff0bfc49f1eb7abc36ed24c9a18d40504bd0
Wagner Amaral <wagner.amaral@nubank.com.br>
2017-12-06 12:57:35 -0200

    2017/3.2

--- 2017/Day3.hs
+++ 2017/Day3.hs
@@ -1,11 +1,22 @@
 module Day3 where
 
-import           Data.List (sort)
+import           Control.Lens
+import           Data.ByteString.Lazy.Char8 (unpack)
+import           Data.List.Split            (splitWhen)
+import           Network.Wreq               (get, responseBody)
+
+readAsInt :: String -> Int
+readAsInt x = read x :: Int
 
 layers :: [Int]
 layers = foldl (\acc x -> acc ++ [x * 4 - 4 + last acc]) [1] [3,5..999]
 
-foo limit x = if (x - limit) < 0 then x else foo limit (x - limit)
+-- That's cheating, right?
+getOeis :: IO [Int]
+getOeis = do
+  res <- get "https://oeis.org/A141481/b141481.txt"
+  let body = res ^. responseBody
+  return $ map (readAsInt . last . words) $ take 100 $ drop 2 $ lines $ unpack body
 
 main :: IO ()
 main = do
@@ -13,7 +24,8 @@ main = do
   let layerSteps = length $ takeWhile (< input) layers
   let currentOffset = input - layers !! (layerSteps - 1)
   let distanceFromAxis = currentOffset `mod` layerSteps
+  oeis <- getOeis
   putStr "1. "
   print $ layerSteps + distanceFromAxis
-  -- putStr "2. "
-  -- print ""
+  putStr "2. "
+  print $ head $ last $ splitWhen (< input) oeis
