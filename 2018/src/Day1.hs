6b426e190fb79460b5a570983e1e4fc18e2bc1f9
Wagner Amaral <wamaral@wamaral.org>
2018-12-21 17:25:01 -0200

    [2018] 1.1

--- 2018/src/Day1.hs
+++ 2018/src/Day1.hs
@@ -2,5 +2,28 @@ module Day1
   (day1part1)
   where
 
+import           Common
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Operation = Plus | Minus deriving Show
+data Frequency = Frequency Operation Int deriving Show
+
+frequencyParser :: Parser Frequency
+frequencyParser = do
+  sign <- char '+' <|> char '-'
+  nums <- stringToInt <$> some digitChar
+  optional newline
+  let op = if sign == '+' then Plus else Minus
+  pure $ Frequency op (fromMaybe 0 nums)
+
+allFrequencies :: String -> [Frequency]
+allFrequencies input = fromMaybe [] $ parseMaybe (someTill frequencyParser eof) input
+
 day1part1 :: String -> String
-day1part1 _ = "1.1"
+day1part1 input = show $ foldl sumFreq 0 $ allFrequencies input
+  where
+    sumFreq count (Frequency op x) = case op of
+      Plus  -> count + x
+      Minus -> count - x
