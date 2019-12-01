b0c8d26748c0ad351286b7f9b4fba88bfe4efca6
Wagner Amaral <wamaral@wamaral.org>
2019-12-01 06:32:07 +0100

    Extract parsers to Common

--- 2019/src/Common.hs
+++ 2019/src/Common.hs
@@ -1,11 +1,18 @@
 module Common where
 
 import           Control.Exception
+import           Data.Maybe
 import           Data.Void
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
 type Parser = Parsec Void String
+
+intParser :: Parser Int
+intParser = fromMaybe 0 . stringToInt <$> some digitChar
+
+readListOf :: Parser a -> String -> [a]
+readListOf parser = fromMaybe [] . parseMaybe (someTill (parser <* optional newline) eof)
 
 stringToInt :: String -> Maybe Int
 stringToInt s = read <$> parseMaybe parser s
--- 2019/src/Day1.hs
+++ 2019/src/Day1.hs
@@ -3,15 +3,6 @@ module Day1
   where
 
 import           Common
-import           Data.Maybe
-import           Text.Megaparsec
-import           Text.Megaparsec.Char
-
-moduleParser :: Parser Int
-moduleParser = fromMaybe 0 . stringToInt <$> some digitChar <* optional newline
-
-parseModules :: String -> [Int]
-parseModules = fromMaybe [] . parseMaybe (someTill moduleParser eof)
 
 fuelRequired :: Int -> Int
 fuelRequired m = maximum [0, (m `div` 3) - 2]
@@ -23,7 +14,7 @@ fuelRequiredWithFuel x
   where required = fuelRequired x
 
 day1part1 :: String -> String
-day1part1 = show . sum . map fuelRequired . parseModules
+day1part1 = show . sum . map fuelRequired . readListOf intParser
 
 day1part2 :: String -> String
-day1part2 = show . sum . map fuelRequiredWithFuel . parseModules
+day1part2 = show . sum . map fuelRequiredWithFuel . readListOf intParser
