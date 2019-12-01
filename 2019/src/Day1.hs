788d8cdb064f674f2b0adf42624525f31c1e6d1d
Wagner Amaral <wamaral@wamaral.org>
2019-12-01 06:24:57 +0100

    2019/1.2

--- 2019/src/Day1.hs
+++ 2019/src/Day1.hs
@@ -14,10 +14,16 @@ parseModules :: String -> [Int]
 parseModules = fromMaybe [] . parseMaybe (someTill moduleParser eof)
 
 fuelRequired :: Int -> Int
-fuelRequired m = (m `div` 3) - 2
+fuelRequired m = maximum [0, (m `div` 3) - 2]
+
+fuelRequiredWithFuel :: Int -> Int
+fuelRequiredWithFuel x
+  | x <= 0    = x
+  | otherwise = required + fuelRequiredWithFuel required
+  where required = fuelRequired x
 
 day1part1 :: String -> String
 day1part1 = show . sum . map fuelRequired . parseModules
 
 day1part2 :: String -> String
-day1part2 _input = "1-2"
+day1part2 = show . sum . map fuelRequiredWithFuel . parseModules
