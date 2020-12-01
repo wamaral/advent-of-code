18ce5ee65ca4946b9f18010bc84a7ad7616fa2c0
Wagner Amaral <wamaral@wamaral.org>
2020-12-01 20:23:07 +0100

    2020/1.1

--- 2020/src/Day1.hs
+++ 2020/src/Day1.hs
@@ -3,9 +3,22 @@ module Day1
   where
 
 import           Common
+import           Data.Function
+import           Data.List
+import           Data.Maybe
+import           Safe
+
+allPairs :: [Int] -> [(Int, Int)]
+allPairs xs = nub [(x,y) | x <- xs, y <- xs, x /= y]
 
 day1part1 :: String -> String
-day1part1 _ = ""
+day1part1 input = readListOf intParser input
+  & allPairs
+  & filter (\(x,y) -> x + y == 2020)
+  & map (\(x,y) -> x * y)
+  & headMay
+  & fromMaybe 0
+  & show
 
 day1part2 :: String -> String
 day1part2 _ = ""
