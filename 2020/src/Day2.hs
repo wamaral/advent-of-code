7dd9310047133e5178a07c095a97b07331b6c469
Wagner Amaral <wamaral@wamaral.org>
2020-12-02 14:48:47 +0100

    2020/2.1

--- 2020/src/Day2.hs
+++ 2020/src/Day2.hs
@@ -1,11 +1,40 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day2
   (day2part1, day2part2)
   where
 
 import           Common
+import           Data.Function
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Policy = Policy
+  { minimumCount :: Int
+  , maximumCount :: Int
+  , letter       :: Char
+  , password     :: String
+  }
+
+policyParser :: Parser Policy
+policyParser = do
+  minimumCount <- intParser
+  _ <- char '-'
+  maximumCount <- intParser
+  _ <- space
+  letter <- letterChar
+  _ <- string ": "
+  password <- many letterChar
+  return $ Policy{..}
+
+isValid :: Policy -> Bool
+isValid p = password p
+  & filter (== letter p)
+  & length
+  & (\l -> l >= minimumCount p && l <= maximumCount p)
 
 day2part1 :: String -> String
-day2part1 _ = ""
+day2part1 = show . length . filter isValid . readListOf policyParser
 
 day2part2 :: String -> String
 day2part2 _ = ""
