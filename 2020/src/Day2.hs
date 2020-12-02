8c6d409f8b6f79b3c79dd48f78d954864e865e07
Wagner Amaral <wamaral@wamaral.org>
2020-12-02 15:05:29 +0100

    2020/2.2

--- 2020/src/Day2.hs
+++ 2020/src/Day2.hs
@@ -6,35 +6,45 @@ module Day2
 
 import           Common
 import           Data.Function
+import           Data.Maybe
+import           Safe
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
 data Policy = Policy
-  { minimumCount :: Int
-  , maximumCount :: Int
-  , letter       :: Char
-  , password     :: String
+  { lower    :: Int
+  , higher   :: Int
+  , letter   :: Char
+  , password :: String
   }
 
 policyParser :: Parser Policy
 policyParser = do
-  minimumCount <- intParser
+  lower <- intParser
   _ <- char '-'
-  maximumCount <- intParser
+  higher <- intParser
   _ <- space
   letter <- letterChar
   _ <- string ": "
   password <- many letterChar
   return $ Policy{..}
 
-isValid :: Policy -> Bool
-isValid p = password p
+isValidPart1 :: Policy -> Bool
+isValidPart1 p = password p
   & filter (== letter p)
   & length
-  & (\l -> l >= minimumCount p && l <= maximumCount p)
+  & (\l -> l >= lower p && l <= higher p)
+
+isValidPart2 :: Policy -> Bool
+isValidPart2 p = [(lower p), (higher p)]
+  & map pred
+  & mapMaybe (atMay (password p))
+  & filter (== letter p)
+  & length
+  & (== 1)
 
 day2part1 :: String -> String
-day2part1 = show . length . filter isValid . readListOf policyParser
+day2part1 = show . length . filter isValidPart1 . readListOf policyParser
 
 day2part2 :: String -> String
-day2part2 _ = ""
+day2part2 = show . length . filter isValidPart2 . readListOf policyParser
