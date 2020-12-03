c68a71d2191068e19e70add475cecace14af32c3
Wagner Amaral <wamaral@wamaral.org>
2020-12-03 18:47:25 +0100

    2020/3.1

--- 2020/src/Day3.hs
+++ 2020/src/Day3.hs
@@ -3,9 +3,24 @@ module Day3
   where
 
 import           Common
+import           Data.Maybe
+import           Safe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Square = Open | Tree deriving (Eq, Show)
+
+squareParser :: Parser Square
+squareParser = choice
+  [ Open <$ char '.'
+  , Tree <$ char '#'
+  ]
+
+squareAtPos :: Int -> [Square] -> Maybe Square
+squareAtPos n squares = atMay (cycle squares) n
 
 day3part1 :: String -> String
-day3part1 _ = ""
+day3part1 = show . length . filter (== Tree) . mapMaybe (uncurry squareAtPos) . zip [3,6..] . tail . readListOf (many squareParser)
 
 day3part2 :: String -> String
 day3part2 _ = ""
