94395dcf1eb54253e3bf4b4400c4e093216aafad
Wagner Amaral <wamaral@wamaral.org>
2020-12-03 18:20:34 +0100

    2020/3.prepare

--- 2020/app/Main.hs
+++ 2020/app/Main.hs
@@ -4,13 +4,14 @@ import           Common
 import qualified Data.Map.Strict as Map
 import           Day1
 import           Day2
+import           Day3
 import           System.IO
 
 completeParts :: Map.Map Int [String -> String]
 completeParts = Map.fromList
   [ (1, [day1part1, day1part2])
   , (2, [day2part1, day2part2])
-  -- , (3, [day3part1, day3part2])
+  , (3, [day3part1, day3part2])
   -- , (4, [day4part1, day4part2])
   ]
 
---
+++ 2020/src/Day3.hs
@@ -0,0 +1,11 @@
+module Day3
+  (day3part1, day3part2)
+  where
+
+import           Common
+
+day3part1 :: String -> String
+day3part1 _ = ""
+
+day3part2 :: String -> String
+day3part2 _ = ""
