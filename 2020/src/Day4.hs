49e663e10b32257cd4f433f95651b446105564fa
Wagner Amaral <wamaral@wamaral.org>
2020-12-05 01:35:46 +0100

    2020/4.1

--- 2020/src/Day4.hs
+++ 2020/src/Day4.hs
@@ -3,9 +3,35 @@ module Day4
   where
 
 import           Common
+import qualified Data.Map             as M
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Passport = M.Map String String
+
+inputParser :: Parser [Passport]
+inputParser = passportParser `sepBy` newline
+
+passportParser :: Parser Passport
+passportParser = M.fromList <$> passportFieldParser `sepEndBy` separator
+  where separator = try $ newline <|> spaceChar
+
+passportFieldParser :: Parser (String, String)
+passportFieldParser = do
+  k <- choice $ map (try . string) ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+  _ <- char ':'
+  v <- some (alphaNumChar <|> char '#')
+  return (k, v)
+
+parseInput :: String -> [Passport]
+parseInput = fromMaybe [] . parseMaybe inputParser
+
+requiredFieldsPresent :: Passport -> Bool
+requiredFieldsPresent c = all (== True) $ map (\x -> M.member x c) ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 day4part1 :: String -> String
-day4part1 _ = ""
+day4part1 = show . length . filter requiredFieldsPresent . parseInput
 
 day4part2 :: String -> String
 day4part2 _ = ""
