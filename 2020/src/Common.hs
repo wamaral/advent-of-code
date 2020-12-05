ed2416f9d65117ecfa2e99d875e6a3df0db05ef0
Wagner Amaral <wamaral@wamaral.org>
2020-12-05 02:17:41 +0100

    2020/4.2

--- 2020/src/Common.hs
+++ 2020/src/Common.hs
@@ -9,7 +9,7 @@ import           Text.Megaparsec.Char
 type Parser = Parsec Void String
 
 intParser :: Parser Int
-intParser = fromMaybe 0 . stringToInt <$> some digitChar
+intParser = stringToInt0 <$> some digitChar
 
 readListOf :: Parser a -> String -> [a]
 readListOf parser = fromMaybe [] . parseMaybe (someTill (parser <* optional newline) eof)
@@ -17,6 +17,9 @@ readListOf parser = fromMaybe [] . parseMaybe (someTill (parser <* optional newl
 stringToInt :: String -> Maybe Int
 stringToInt s = read <$> parseMaybe parser s
   where parser = many digitChar :: Parser String
+
+stringToInt0 :: String -> Int
+stringToInt0 = fromMaybe 0 . stringToInt
 
 inputNotFound :: IOError -> IO String
 inputNotFound _ = pure ""
--- 2020/src/Day4.hs
+++ 2020/src/Day4.hs
@@ -30,8 +30,32 @@ parseInput = fromMaybe [] . parseMaybe inputParser
 requiredFieldsPresent :: Passport -> Bool
 requiredFieldsPresent c = all (== True) $ map (\x -> M.member x c) ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
+validHeight :: String -> Bool
+validHeight s = case scale of
+  "cm" -> height >= 150 && height <= 193
+  "in" -> height >= 59 && height <= 76
+  _    -> False
+  where
+    (height, scale) = fromMaybe (0, "") $ parseMaybe heightParser s
+    heightParser :: Parser (Int, String)
+    heightParser = (,) <$> intParser <*> choice (map string ["cm", "in"])
+
+validField :: String -> String -> Bool
+validField "byr" v = (\x -> x >= 1920 && x <= 2002) . stringToInt0 $ v
+validField "iyr" v = (\x -> x >= 2010 && x <= 2020) . stringToInt0 $ v
+validField "eyr" v = (\x -> x >= 2020 && x <= 2030) . stringToInt0 $ v
+validField "hgt" v = validHeight v
+validField "hcl" v = isJust $ parseMaybe (char '#' >> count 6 hexDigitChar :: Parser String) v
+validField "ecl" v = v `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+validField "pid" v = isJust $ parseMaybe (count 9 digitChar :: Parser String) v
+validField "cid" _ = True
+validField _ _     = False
+
+validFields :: Passport -> Bool
+validFields = all (== True) . M.elems . M.mapWithKey validField
+
 day4part1 :: String -> String
 day4part1 = show . length . filter requiredFieldsPresent . parseInput
 
 day4part2 :: String -> String
-day4part2 _ = ""
+day4part2 = show . length . filter validFields . filter requiredFieldsPresent . parseInput
