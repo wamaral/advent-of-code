9ed45bfde03760c3e3f86bd2b044b636b1e84425
Wagner Amaral <wamaral@wamaral.org>
2020-12-06 15:10:37 +0100

    2020/6.1

--- 2020/src/Day6.hs
+++ 2020/src/Day6.hs
@@ -2,10 +2,11 @@ module Day6
   (day6part1, day6part2)
   where
 
-import           Common
+import           Data.List
+import           Data.List.Split
 
 day6part1 :: String -> String
-day6part1 _ = ""
+day6part1 = show . sum . map (length . foldr1 union) . splitOn [""] . lines
 
 day6part2 :: String -> String
 day6part2 _ = ""
