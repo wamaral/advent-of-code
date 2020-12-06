e70e27c784a8beb5e22c0ddc2489e377a535b5c2
Wagner Amaral <wamaral@wamaral.org>
2020-12-06 01:44:42 +0100

    2020/5.prepare

--- 2020/app/Main.hs
+++ 2020/app/Main.hs
@@ -6,6 +6,7 @@ import           Day1
 import           Day2
 import           Day3
 import           Day4
+import           Day5
 import           System.IO
 
 completeParts :: Map.Map Int [String -> String]
@@ -14,6 +15,7 @@ completeParts = Map.fromList
   , (2, [day2part1, day2part2])
   , (3, [day3part1, day3part2])
   , (4, [day4part1, day4part2])
+  , (5, [day5part1, day5part2])
   ]
 
 main :: IO ()
---
+++ 2020/src/Day5.hs
@@ -0,0 +1,11 @@
+module Day5
+  (day5part1, day5part2)
+  where
+
+import           Common
+
+day5part1 :: String -> String
+day5part1 _ = ""
+
+day5part2 :: String -> String
+day5part2 _ = ""
