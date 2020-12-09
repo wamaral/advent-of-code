68597b05877f54ba26e6a7966cef165e848b27c9
Wagner Amaral <wamaral@wamaral.org>
2020-12-09 17:23:16 +0100

    2020/9.1

--- 2020/src/Day9.hs
+++ 2020/src/Day9.hs
@@ -2,8 +2,22 @@ module Day9
   (day9part1, day9part2)
   where
 
+import           Common
+import           Control.Monad
+import           Data.Function
+import           Data.List.Split
+import           Safe
+
+isSumOfAnyPair :: Int -> [Int] -> Bool
+isSumOfAnyPair x xs = x `elem` allSums
+  where allSums = map sum $ replicateM 2 xs
+
+notSums :: [Int] -> [Int]
+notSums xs = map fst $ filter (not . uncurry isSumOfAnyPair) allTests
+  where allTests = divvy 26 1 xs & map ((\(y:ys) -> (y, ys)) . reverse)
+
 day9part1 :: String -> String
-day9part1 _ = ""
+day9part1 = show . headDef 0 . notSums . readListOf intParser
 
 day9part2 :: String -> String
 day9part2 _ = ""
