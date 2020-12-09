d4822f60d5fc5b30e7e8ffee7100b52b3bb59237
Wagner Amaral <wamaral@wamaral.org>
2020-12-09 18:24:35 +0100

    2020/9.2

--- 2020/src/Day9.hs
+++ 2020/src/Day9.hs
@@ -5,6 +5,7 @@ module Day9
 import           Common
 import           Control.Monad
 import           Data.Function
+import           Data.List
 import           Data.List.Split
 import           Safe
 
@@ -16,8 +17,27 @@ notSums :: [Int] -> [Int]
 notSums xs = map fst $ filter (not . uncurry isSumOfAnyPair) allTests
   where allTests = divvy 26 1 xs & map ((\(y:ys) -> (y, ys)) . reverse)
 
+contiguousSumUntil :: Int -> [Int] -> [Int]
+contiguousSumUntil n xs = scanl' (+) 0 xs
+  & tail
+  & takeWhile (<= n)
+  & length
+  & (`take` xs)
+
+contiguousSumEqualTo :: Int -> [Int] -> [Int]
+contiguousSumEqualTo n xs = map (contiguousSumUntil n) allTests
+  & filter (\ys -> length ys >= 2)
+  & filter (\ys -> n == sum ys)
+  & headDef []
+  where allTests = tails xs
+
 day9part1 :: String -> String
 day9part1 = show . headDef 0 . notSums . readListOf intParser
 
 day9part2 :: String -> String
-day9part2 _ = ""
+day9part2 input = contiguousSumEqualTo invalidNum inputList
+  & (\xs -> minimum xs + maximum xs)
+  & show
+  where
+    inputList = readListOf intParser input
+    invalidNum = headDef 0 $ notSums inputList
