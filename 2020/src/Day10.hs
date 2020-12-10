c8aa2aaba8aa9f777f8d83b36fcec90edb4faa3c
Wagner Amaral <wamaral@wamaral.org>
2020-12-10 23:13:04 +0100

    2020/10.1

--- 2020/src/Day10.hs
+++ 2020/src/Day10.hs
@@ -2,8 +2,14 @@ module Day10
   (day10part1, day10part2)
   where
 
+import           Common
+import           Data.List
+
 day10part1 :: String -> String
-day10part1 _ = ""
+day10part1 = show . product . map length . group . sort . map (\(x,y) -> y - x) . pairs . filledList . sort . readListOf intParser
+  where
+    filledList xs = [0] ++ xs ++ [maximum xs + 3]
+    pairs xs = zip xs (tail xs)
 
 day10part2 :: String -> String
 day10part2 _ = ""
