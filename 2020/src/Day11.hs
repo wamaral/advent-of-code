b1891ccef96e5986aaf078c2cec59272b51c7c0c
Wagner Amaral <wamaral@wamaral.org>
2020-12-12 04:16:29 +0100

    2020/11.2

--- 2020/src/Day11.hs
+++ 2020/src/Day11.hs
@@ -7,6 +7,7 @@ import           Data.Function
 import qualified Data.Map             as M
 import           Data.Maybe
 import           Linear.V2
+import           Safe
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
@@ -24,30 +25,45 @@ seatParser = choice
 inputToGrid :: [[Seat]] -> Grid
 inputToGrid xs = foldl (\m (y, seats) -> foldl (\m' (x, seat) -> M.insert (V2 x y) seat m') m seats) M.empty $ zip [0..] $ map (zip [0..]) xs -- ouch
 
-adjacentIndexes :: Coord -> [Coord]
-adjacentIndexes v = map (v +) adjList
-  where adjList = [V2 (-1) (-1), V2 0 (-1), V2 1 (-1),
-                   V2 (-1) 0,               V2 1 0,
-                   V2 (-1) 1,    V2 0 1,    V2 1 1]
+adjacentDirections :: [V2 Int]
+adjacentDirections = [V2 (-1) (-1), V2 0 (-1), V2 1 (-1),
+                      V2 (-1) 0,               V2 1 0,
+                      V2 (-1) 1,    V2 0 1,    V2 1 1]
 
-adjacents :: Grid -> Coord -> [Seat]
-adjacents grid coord = mapMaybe (`M.lookup` grid) $ adjacentIndexes coord
+kingAdjacentIndexes :: Coord -> [Coord]
+kingAdjacentIndexes coord = map (+ coord) adjacentDirections
 
-changeSeat :: Seat -> [Seat] -> Seat
-changeSeat Empty adj    = if Occupied `notElem` adj then Occupied else Empty
-changeSeat Occupied adj = if filter (== Occupied) adj & length & (>= 4) then Empty else Occupied
-changeSeat seat _       = seat
+kingAdjacents :: Grid -> Coord -> [Seat]
+kingAdjacents grid coord = mapMaybe (`M.lookup` grid) $ kingAdjacentIndexes coord
 
-changeGrid :: Grid -> Grid
-changeGrid grid = M.mapWithKey f grid
-  where f coord seat = changeSeat seat (adjacents grid coord)
+infiniteQueenAdjacentIndexes :: Coord -> [[Coord]]
+infiniteQueenAdjacentIndexes coord = map (\x -> tail $ iterate (+ x) coord) adjacentDirections
 
-fixpoint :: Grid -> Grid
-fixpoint grid = if grid == newGrid then grid else fixpoint newGrid
-  where newGrid = changeGrid grid
+queenAdjacents :: Grid -> Coord -> [Seat]
+queenAdjacents grid coord = map whatCanISee $ infiniteQueenAdjacentIndexes coord
+  where whatCanISee coords = map (`M.lookup` grid) coords & takeWhile isJust & catMaybes & filter (/= Floor) & headDef Floor
+
+changeSeat :: Seat -> [Seat] -> Int -> Seat
+changeSeat Empty adj _             = if Occupied `notElem` adj then Occupied else Empty
+changeSeat Occupied adj neighbours = if filter (== Occupied) adj & length & (>= neighbours) then Empty else Occupied
+changeSeat seat _ _                = seat
+
+changeGrid :: (Grid -> Coord -> [Seat]) -> Int -> Grid -> Grid
+changeGrid f x grid = M.mapWithKey f' grid
+  where f' coord seat = changeSeat seat (f grid coord) x
+
+changeGridKing :: Grid -> Grid
+changeGridKing = changeGrid kingAdjacents 4
+
+changeGridQueen :: Grid -> Grid
+changeGridQueen = changeGrid queenAdjacents 5
+
+fixpoint :: (Grid -> Grid) -> Grid -> Grid
+fixpoint f grid = if grid == newGrid then grid else fixpoint f newGrid
+  where newGrid = f grid
 
 day11part1 :: String -> String
-day11part1 = show . M.size . M.filter (== Occupied) . fixpoint . inputToGrid . readListOf (many seatParser)
+day11part1 = show . M.size . M.filter (== Occupied) . fixpoint changeGridKing . inputToGrid . readListOf (many seatParser)
 
 day11part2 :: String -> String
-day11part2 _ = ""
+day11part2 = show . M.size . M.filter (== Occupied) . fixpoint changeGridQueen . inputToGrid . readListOf (many seatParser)
