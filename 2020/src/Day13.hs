f5390c6c5d9714b870ddb8e031315515ff8937f2
Wagner Amaral <wamaral@wamaral.org>
2020-12-14 01:22:55 +0100

    2020/13.2

--- 2020/src/Day13.hs
+++ 2020/src/Day13.hs
@@ -13,18 +13,18 @@ import           Safe
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
-type MyDepart = Int
-type Bus = Int
+type MyDepart = Integer
+type Bus = Integer
 
 busParser :: Parser (Maybe Bus)
 busParser = choice
-  [ Just <$> intParser
+  [ Just . toInteger <$> intParser
   , Nothing <$ char 'x'
   ]
 
 inputParser :: Parser (MyDepart, [Maybe Bus])
 inputParser = do
-  myDepart <- intParser
+  myDepart <- toInteger <$> intParser
   _ <- newline
   busIds <- busParser `sepBy` char ','
   _ <- optional newline
@@ -34,11 +34,24 @@ inputParser = do
 parseInput :: String -> (MyDepart, [Maybe Bus])
 parseInput = fromMaybe (0, []) . parseMaybe inputParser
 
-busSchedule :: Bus -> [Int]
+busSchedule :: Bus -> [Integer]
 busSchedule b = [0,b..]
 
-nextScheduleAfterMyDepart :: MyDepart -> Bus -> (Bus, Int)
+nextScheduleAfterMyDepart :: MyDepart -> Bus -> (Bus, Integer)
 nextScheduleAfterMyDepart myDepart bus = (bus,) <$> headDef 0 $ dropWhile (< myDepart) (busSchedule bus)
+
+indexBusses :: [Maybe Bus] -> [(Integer, Bus)]
+indexBusses bs = zip [0..] bs & mapMaybe flipTheBus
+  where
+    flipTheBus (_, Nothing) = Nothing
+    flipTheBus (x, Just b)  = Just (x,b)
+
+matchingSchedule :: (Integer, [Bus]) -> (Integer, Bus) -> (Integer, [Bus])
+matchingSchedule (period, schedule) (busIndex, bus) = (newPeriod, newSchedule)
+  where
+    newOffset = find (\x -> (x + busIndex) `mod` bus == 0) schedule & fromMaybe bus
+    newPeriod = if period == 0 then bus else lcm period bus
+    newSchedule = [newOffset, newOffset + newPeriod ..]
 
 day13part1 :: String -> String
 day13part1 input = catMaybes busses
@@ -49,4 +62,10 @@ day13part1 input = catMaybes busses
   where (myDepart, busses) = parseInput input
 
 day13part2 :: String -> String
-day13part2 _ = ""
+day13part2 input = parseInput input
+  & snd
+  & indexBusses
+  & foldl matchingSchedule (0, [])
+  & snd
+  & headDef 0
+  & show
