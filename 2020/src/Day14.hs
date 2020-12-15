ca3bb3997fac9431a0d282bf37fd176e288ff212
Wagner Amaral <wamaral@wamaral.org>
2020-12-15 02:19:49 +0100

    2020/14.2

--- 2020/src/Day14.hs
+++ 2020/src/Day14.hs
@@ -5,11 +5,12 @@ module Day14
 import           Common
 import           Data.Bits
 import           Data.Function
+import           Data.List
 import qualified Data.Map             as M
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
-data MaskBit = On | Off | Pass deriving Show
+data MaskBit = On | Off | Pass deriving (Eq, Show)
 type Mask = [MaskBit]
 type Address = Int
 type Value = Int
@@ -41,19 +42,50 @@ newProgram = Program {mask = replicate 36 Pass, memory = M.empty}
 maskValue :: Value -> Mask -> Value
 maskValue val m = reverse m
   & zip [0..]
-  & foldl updateBit val
+  & foldl' updateBit val
   where
     updateBit :: Value -> (Int, MaskBit) -> Value
     updateBit v (i, On)   = setBit v i
     updateBit v (i, Off)  = clearBit v i
     updateBit v (_, Pass) = v
 
+addressToMask :: Address -> Mask
+addressToMask addr = reverse $ map (\i -> if testBit addr i then On else Off) [0..35]
+
+mergeMaskBit :: MaskBit -> MaskBit -> MaskBit
+mergeMaskBit b1 Off = b1
+mergeMaskBit _ b2   = b2
+
+allCombinations :: Mask -> Mask -> [Address]
+allCombinations m1 m2 = foldl' duplicateBits [addr1] [0..35]
+  where
+    addr1 = maskValue 0 m1
+    addr2 = maskValue 0 m2
+    duplicateBits addrs i = addrs ++ if testBit addr1 i == testBit addr2 i then [] else map (`complementBit` i) addrs
+
+maskAddresses :: Address -> Mask -> [Address]
+maskAddresses addr m = addressToMask addr
+  & zipWith (flip mergeMaskBit) m
+  & (\x -> (turn On x, turn Off x))
+  & uncurry allCombinations
+  where
+    turn b = map (\x -> if x == Pass then b else x)
+
 runInstruction :: Program -> Instruction -> Program
 runInstruction prog (SetMask m) = prog {mask = m}
-runInstruction prog (SetMem addr v) = prog {memory = M.insert addr (maskValue v (mask prog)) (memory prog)}
+runInstruction prog (SetMem addr v) = prog {memory = newMemory}
+  where newMemory = M.insert addr (maskValue v (mask prog)) (memory prog)
+
+runInstructionV2 :: Program -> Instruction -> Program
+runInstructionV2 prog (SetMask m) = prog {mask = m}
+runInstructionV2 prog (SetMem addr v) = prog {memory = newMemory}
+  where newMemory = foldl' (\m a -> M.insert a v m) (memory prog) (maskAddresses addr (mask prog))
+
+runProgram :: (Program -> Instruction -> Program) -> String -> String
+runProgram p = show . sum . M.elems . memory . foldl' p newProgram . readListOf instructionParser
 
 day14part1 :: String -> String
-day14part1 = show . sum . M.elems . memory . foldl runInstruction newProgram . readListOf instructionParser
+day14part1 = runProgram runInstruction
 
 day14part2 :: String -> String
-day14part2 _ = ""
+day14part2 = runProgram runInstructionV2
