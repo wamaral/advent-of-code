d189a47841c056cf68e9de3298c587149949d4b4
Wagner Amaral <wamaral@wamaral.org>
2020-12-16 01:51:24 +0100

    2020/15.2

--- 2020/src/Day15.hs
+++ 2020/src/Day15.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Strict #-}
+
 module Day15
   (day15part1, day15part2)
   where
@@ -5,27 +7,50 @@ module Day15
 import           Common
 import           Data.Function
 import           Data.List
+import qualified Data.Map             as M
 import           Data.Maybe
 import           Safe
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
+data Game = Game
+  { index   :: Int
+  , number  :: Int
+  , visited :: M.Map Int Int
+  } deriving Show
+
 inputParser :: Parser [Int]
 inputParser = intParser `sepBy` char ',' <* optional newline
 
-nextNumber :: [Int] -> [Int]
-nextNumber xs = xs ++ [prevN]
+prefillGame :: [Int] -> Game
+prefillGame xs = Game
+  { index = length xs
+  , number = lastDef minBound xs
+  , visited = foldl' (\m (i, x) -> M.insert x i m) M.empty $ zip [1..(pred $ length xs)] xs
+  }
+
+nextNumber :: Game -> Game
+nextNumber game = Game
+  { index = newIndex
+  , number = newNumber
+  , visited = M.insert (number game) (index game) (visited game)
+  }
   where
-    n = lastDef 0 xs
-    prevN = reverse xs & tailDef [] & elemIndex n & fmap succ & fromMaybe 0
+    newIndex = succ $ index game
+    newNumber = M.lookup (number game) (visited game) & fmap (\x -> index game - x) & fromMaybe 0
+
+runFor :: Int -> String -> String
+runFor n input = iterate nextNumber baseGame
+  & take (n - pred (length parsedInput))
+  & last
+  & number
+  & show
+  where
+    parsedInput = fromMaybe [] $ parseMaybe inputParser input
+    baseGame = prefillGame parsedInput
 
 day15part1 :: String -> String
-day15part1 input = iterate nextNumber parsedInput
-  & take (2020 - pred (length parsedInput))
-  & last
-  & last
-  & show
-  where parsedInput = fromMaybe [] $ parseMaybe inputParser input
+day15part1 = runFor 2020
 
 day15part2 :: String -> String
-day15part2 _ = ""
+day15part2 = runFor 30000000
