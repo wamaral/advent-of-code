a33688b5d5d4cf7dec708de5df70d46be9f7cffc
Wagner Amaral <wamaral@wamaral.org>
2020-12-16 01:51:09 +0100

    2020/15.2.test

--- 2020/test/Day15Spec.hs
+++ 2020/test/Day15Spec.hs
@@ -16,3 +16,12 @@ spec = do
       day15part1 "2,3,1" `shouldBe` "78"
       day15part1 "3,2,1" `shouldBe` "438"
       day15part1 "3,1,2" `shouldBe` "1836"
+  describe "part 2" $ do
+    it "runs provided examples" $ do
+      day15part2 "0,3,6" `shouldBe` "436"
+      day15part2 "1,3,2" `shouldBe` "2578"
+      day15part2 "2,1,3" `shouldBe` "3544142"
+      day15part2 "1,2,3" `shouldBe` "261214"
+      day15part2 "2,3,1" `shouldBe` "6895259"
+      day15part2 "3,2,1" `shouldBe` "18"
+      day15part2 "3,1,2" `shouldBe` "362"
