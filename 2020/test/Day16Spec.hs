aaa9725c731e3d561514788fd9045161c017d071
Wagner Amaral <wamaral@wamaral.org>
2020-12-17 00:12:24 +0100

    2020/16.2.test

--- 2020/test/Day16Spec.hs
+++ 2020/test/Day16Spec.hs
@@ -5,8 +5,8 @@ module Day16Spec
 import           Day16
 import           Test.Hspec
 
-input :: String
-input = unlines
+input1 :: String
+input1 = unlines
   [ "class: 1-3 or 5-7"
   , "row: 6-11 or 33-44"
   , "seat: 13-40 or 45-50"
@@ -21,8 +21,26 @@ input = unlines
   , "38,6,12"
   ]
 
+input2 :: String
+input2 = unlines
+  [ "departure class: 0-1 or 4-19"
+  , "departure row: 0-5 or 8-19"
+  , "seat: 0-13 or 16-19"
+  , ""
+  , "your ticket:"
+  , "11,12,13"
+  , ""
+  , "nearby tickets:"
+  , "3,9,18"
+  , "15,1,5"
+  , "5,14,9"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
-      day16part1 input `shouldBe` "71"
+      day16part1 input1 `shouldBe` "71"
+  describe "part 2" $ do
+    it "runs provided example" $ do
+      day16part2 input2 `shouldBe` "132"
