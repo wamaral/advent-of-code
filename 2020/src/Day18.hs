0ddd740b02a9276ec8e61b89ece5883102704233
Wagner Amaral <wamaral@wamaral.org>
2020-12-19 01:56:54 +0100

    2020/18.2

--- 2020/src/Day18.hs
+++ 2020/src/Day18.hs
@@ -21,17 +21,23 @@ parens = between (char '(') (char ')')
 binary :: Char -> (Expr -> Expr -> Expr) -> Operator Parser Expr
 binary sym f = InfixL (f <$ skipSpaces (char sym))
 
-termParser :: Parser Expr
-termParser = choice
-  [ skipSpaces $ parens (skipSpaces exprParser)
+termParser :: Parser Expr -> Parser Expr
+termParser exprP = choice
+  [ skipSpaces $ parens (skipSpaces exprP)
   , NumVal <$> skipSpaces intParser
   ]
 
 exprParser :: Parser Expr
-exprParser = makeExprParser termParser operatorTable
+exprParser = makeExprParser (termParser exprParser) operatorTable
+
+exprParser2 :: Parser Expr
+exprParser2 = makeExprParser (termParser exprParser2) operatorTable2
 
 operatorTable :: [[Operator Parser Expr]]
 operatorTable = [[binary '*' Product , binary '+' Sum]]
+
+operatorTable2 :: [[Operator Parser Expr]]
+operatorTable2 = [[binary '+' Sum], [binary '*' Product]]
 
 calculate :: Expr -> Int
 calculate (NumVal x)    = x
@@ -42,4 +48,4 @@ day18part1 :: String -> String
 day18part1 = show . sum . map calculate . readListOf exprParser
 
 day18part2 :: String -> String
-day18part2 _ = ""
+day18part2 = show . sum . map calculate . readListOf exprParser2
