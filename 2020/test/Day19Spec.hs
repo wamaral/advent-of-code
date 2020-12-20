a40ec4d1744f7a4871ac1b80720031dd0782d7f3
Wagner Amaral <wamaral@wamaral.org>
2020-12-20 01:44:07 +0100

    2020/19.2.test

--- 2020/test/Day19Spec.hs
+++ 2020/test/Day19Spec.hs
@@ -34,9 +34,64 @@ input2 = unlines
   , "aaaabbb"
   ]
 
+input3 :: String
+input3 = unlines
+  [ "42: 9 14 | 10 1"
+  , "9: 14 27 | 1 26"
+  , "10: 23 14 | 28 1"
+  , "1: \"a\""
+  , "11: 42 31"
+  , "5: 1 14 | 15 1"
+  , "19: 14 1 | 14 14"
+  , "12: 24 14 | 19 1"
+  , "16: 15 1 | 14 14"
+  , "31: 14 17 | 1 13"
+  , "6: 14 14 | 1 14"
+  , "2: 1 24 | 14 4"
+  , "0: 8 11"
+  , "13: 14 3 | 1 12"
+  , "15: 1 | 14"
+  , "17: 14 2 | 1 7"
+  , "23: 25 1 | 22 14"
+  , "28: 16 1"
+  , "4: 1 1"
+  , "20: 14 14 | 1 15"
+  , "3: 5 14 | 16 1"
+  , "27: 1 6 | 14 18"
+  , "14: \"b\""
+  , "21: 14 1 | 1 14"
+  , "25: 1 1 | 1 14"
+  , "22: 14 14"
+  , "8: 42"
+  , "26: 14 22 | 1 20"
+  , "18: 15 15"
+  , "7: 14 5 | 1 21"
+  , "24: 14 1"
+  , ""
+  , "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa"
+  , "bbabbbbaabaabba"
+  , "babbbbaabbbbbabbbbbbaabaaabaaa"
+  , "aaabbbbbbaaaabaababaabababbabaaabbababababaaa"
+  , "bbbbbbbaaaabbbbaaabbabaaa"
+  , "bbbababbbbaaaaaaaabbababaaababaabab"
+  , "ababaaaaaabaaab"
+  , "ababaaaaabbbaba"
+  , "baabbaaaabbaaaababbaababb"
+  , "abbbbabbbbaaaababbbbbbaaaababb"
+  , "aaaaabbaabaaaaababaa"
+  , "aaaabbaaaabbaaa"
+  , "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa"
+  , "babaaabbbaaabaababbaabababaaab"
+  , "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
       day19part1 input1 `shouldBe` "2"
       day19part1 input2 `shouldBe` "2"
+      day19part1 input3 `shouldBe` "3"
+  describe "part 2" $ do
+    it "runs provided example" $ do
+      day19part2 input3 `shouldBe` "12"
