c0fdc5970eb799836df38a32766102b7ad72e780
Wagner Amaral <wamaral@wamaral.org>
2020-12-23 05:12:48 +0100

    2020/22.2

--- 2020/src/Day22.hs
+++ 2020/src/Day22.hs
@@ -12,6 +12,7 @@ import           Text.Megaparsec.Char
 type Card = Int
 type Hand = [Card]
 type Game = (Hand, Hand)
+type GameList = [Game]
 
 handParser :: Parser Hand
 handParser = do
@@ -40,8 +41,24 @@ runGame = do
     put $ if card1 >= card2 then (h1' ++ [card1, card2], h2') else (h1', h2' ++ [card2, card1])
     runGame
 
+runGame2 :: State GameList Hand
+runGame2 = do
+  allStates <- get
+  let (h1, h2) = headDef ([], []) allStates
+  if (h1, h2) `elem` tailDef [] allStates || null h2 then return h1 else if null h1 then return h2 else do
+      let card1 = headDef 0 h1
+      let card2 = headDef 0 h2
+      let h1' = tailDef [] h1
+      let h2' = tailDef [] h2
+      put $ (: allStates)  $ if card1 > length h1' || card2 > length h2'
+        then if card1 >= card2 then (h1' ++ [card1, card2], h2') else (h1', h2' ++ [card2, card1])
+        else do
+          let (subh1, _subh2) = headDef ([], []) $ execState runGame2 [(take card1 h1', take card2 h2')]
+          if null subh1 then (h1', h2' ++ [card2, card1]) else (h1' ++ [card1, card2], h2')
+      runGame2
+
 day22part1 :: String -> String
 day22part1 = show . sum . zipWith (*) [1..] . reverse . evalState runGame . parseInput
 
 day22part2 :: String -> String
-day22part2 _ = ""
+day22part2 = show . sum . zipWith (*) [1..] . reverse . evalState runGame2 . (: []) . parseInput
