1e02d2fd8043c9aa7711d5689f1c6c4027f15092
Wagner Amaral <wamaral@wamaral.org>
2020-12-23 04:39:24 +0100

    2020/22.1.test

--- 2020/test/Day22Spec.hs
+++ 2020/test/Day22Spec.hs
@@ -5,11 +5,28 @@ module Day22Spec
 import           Day22
 import           Test.Hspec
 
+input :: String
+input = unlines
+  [ "Player 1:"
+  , "9"
+  , "2"
+  , "6"
+  , "3"
+  , "1"
+  , ""
+  , "Player 2:"
+  , "5"
+  , "8"
+  , "4"
+  , "7"
+  , "10"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
-      day22part1 "" `shouldBe` ""
+      day22part1 input `shouldBe` "306"
   describe "part 2" $ do
     it "runs provided example" $ do
       day22part2 "" `shouldBe` ""
