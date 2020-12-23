9b9b315ccd2c5091e9127b5b7d6bb121b3157639
Wagner Amaral <wamaral@wamaral.org>
2020-12-23 04:01:02 +0100

    2020/21.1.test

--- 2020/test/Day21Spec.hs
+++ 2020/test/Day21Spec.hs
@@ -5,11 +5,19 @@ module Day21Spec
 import           Day21
 import           Test.Hspec
 
+input :: String
+input = unlines
+  [ "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)"
+  , "trh fvjkl sbzzf mxmxvkd (contains dairy)"
+  , "sqjhc fvjkl (contains soy)"
+  , "sqjhc mxmxvkd sbzzf (contains fish)"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
-      day21part1 "" `shouldBe` ""
+      day21part1 input `shouldBe` "5"
   describe "part 2" $ do
     it "runs provided example" $ do
       day21part2 "" `shouldBe` ""
