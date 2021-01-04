b363f65c962969199f2e1a42c1b0dce49c7274b6
Wagner Amaral <wamaral@wamaral.org>
2021-01-04 07:57:55 +0100

    2020/24.1.test

--- 2020/test/Day24Spec.hs
+++ 2020/test/Day24Spec.hs
@@ -5,11 +5,35 @@ module Day24Spec
 import           Day24
 import           Test.Hspec
 
+input :: String
+input = unlines
+  [ "sesenwnenenewseeswwswswwnenewsewsw"
+  , "neeenesenwnwwswnenewnwwsewnenwseswesw"
+  , "seswneswswsenwwnwse"
+  , "nwnwneseeswswnenewneswwnewseswneseene"
+  , "swweswneswnenwsewnwneneseenw"
+  , "eesenwseswswnenwswnwnwsewwnwsene"
+  , "sewnenenenesenwsewnenwwwse"
+  , "wenwwweseeeweswwwnwwe"
+  , "wsweesenenewnwwnwsenewsenwwsesesenwne"
+  , "neeswseenwwswnwswswnw"
+  , "nenwswwsewswnenenewsenwsenwnesesenew"
+  , "enewnwewneswsewnwswenweswnenwsenwsw"
+  , "sweneswneswneneenwnewenewwneswswnese"
+  , "swwesenesewenwneswnwwneseswwne"
+  , "enesenwswwswneneswsenwnewswseenwsese"
+  , "wnwnesenesenenwwnenwsewesewsesesew"
+  , "nenewswnwewswnenesenwnesewesw"
+  , "eneswnwswnwsenenwnwnwwseeswneewsenese"
+  , "neswnwewnwnwseenwseesewsenwsweewe"
+  , "wseweeenwnesenwwwswnew"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
-      day24part1 "" `shouldBe` ""
+      day24part1 input `shouldBe` "10"
   describe "part 2" $ do
     it "runs provided example" $ do
       day24part2 "" `shouldBe` ""
