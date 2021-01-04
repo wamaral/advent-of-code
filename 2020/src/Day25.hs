62ccd6d9b12a4d5922b5d8b5a11b38748cefc0af
Wagner Amaral <wamaral@wamaral.org>
2021-01-04 10:01:22 +0100

    2020/25.1

--- 2020/src/Day25.hs
+++ 2020/src/Day25.hs
@@ -2,8 +2,42 @@ module Day25
   (day25part1, day25part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import           Data.Maybe
+import           Safe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+inputParser :: Parser (Int, Int)
+inputParser = do
+  cardPub <- intParser
+  _ <- newline
+  doorPub <- intParser
+  _ <- optional newline
+  _ <- eof
+  return (cardPub, doorPub)
+
+parseInput :: String -> (Int, Int)
+parseInput = fromMaybe (0,0) . parseMaybe inputParser
+
+transform :: Int -> Int -> Int
+transform subject value = (value * subject) `rem` 20201227
+
+transforms :: Int -> [Int]
+transforms subject = tail $ iterate (transform subject) 1
+
+loopCounts :: (Int, Int) -> (Int, Int)
+loopCounts (cardPub, doorPub) = (getCount cardPub, getCount doorPub)
+  where getCount pub = elemIndex pub (transforms 7) & fmap succ & fromMaybe 0
+
+encryptionKey :: (Int, Int) -> Int
+encryptionKey pubs@(_, doorPub) = transforms doorPub & take cardLoop & lastDef 0
+  where (cardLoop, _) = loopCounts pubs
+
 day25part1 :: String -> String
-day25part1 _ = ""
+day25part1 = show . encryptionKey . parseInput
 
 day25part2 :: String -> String
 day25part2 _ = ""
