da948ac3f9329c2c74c47451f21450e9dc502e74
Wagner Amaral <wamaral@wamaral.org>
2021-01-04 09:19:34 +0100

    2020/24.2

--- 2020/src/Day24.hs
+++ 2020/src/Day24.hs
@@ -3,14 +3,18 @@ module Day24
   where
 
 import           Common
+import           Data.Function
 import           Data.List
+import qualified Data.Set             as S
 import           Linear.V3
+import           Safe
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
-data Direction = East | West | Southeast | Southwest | Northeast | Northwest deriving Show
+data Direction = East | West | Southeast | Southwest | Northeast | Northwest deriving (Enum, Show)
 type Tilepath = [Direction]
 type Coord = V3 Int
+type Grid = S.Set Coord
 
 directionParser :: Parser Direction
 directionParser = choice
@@ -37,8 +41,47 @@ directionToCoord Northwest = V3 0    1    (-1)
 walk :: Coord -> Direction -> Coord
 walk tile direction = tile + directionToCoord direction
 
+neighbourCoords :: Coord -> [Coord]
+neighbourCoords coord = map (walk coord) [East .. Northwest]
+
+activeNeighbours :: Coord -> Grid -> Grid
+activeNeighbours coord grid = S.fromList $ filter (`S.member` grid) (neighbourCoords coord)
+
+gridToCheck :: Grid -> Grid
+gridToCheck grid = S.toList grid
+  & concatMap neighbourCoords
+  & S.fromList
+  & S.union grid
+
+flipGrid :: Grid -> Grid
+flipGrid grid = gridToCheck grid
+  & S.filter flipTile
+  where
+    countNeighbours tile = activeNeighbours tile grid & S.size
+    flipTile tile = if S.member tile grid
+      then countNeighbours tile == 1 || countNeighbours tile == 2
+      else countNeighbours tile == 2
+
+initialState :: [Tilepath] -> Grid
+initialState paths = paths
+  & map (foldl' walk (V3 0 0 0))
+  & sort
+  & group
+  & filter (odd . length)
+  & map head
+  & S.fromList
+
 day24part1 :: String -> String
-day24part1 = show . length . filter (odd . length) . group . sort . map (foldl' walk (V3 0 0 0)) . readListOf tilepathParser
+day24part1 input = readListOf tilepathParser input
+  & initialState
+  & S.size
+  & show
 
 day24part2 :: String -> String
-day24part2 _ = ""
+day24part2 input = readListOf tilepathParser input
+  & initialState
+  & iterate flipGrid
+  & take 101
+  & lastDef S.empty
+  & S.size
+  & show
