9e3b165d07d43d81c4893bd9f13721f14f2bfb40
Wagner Amaral <wamaral@wamaral.org>
2021-01-04 10:01:07 +0100

    2020/25.1.test

--- 2020/test/Day25Spec.hs
+++ 2020/test/Day25Spec.hs
@@ -5,11 +5,17 @@ module Day25Spec
 import           Day25
 import           Test.Hspec
 
+input :: String
+input = unlines
+  [ "5764801"
+  , "17807724"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided example" $ do
-      day25part1 "" `shouldBe` ""
+      day25part1 input `shouldBe` "14897079"
   describe "part 2" $ do
     it "runs provided example" $ do
       day25part2 "" `shouldBe` ""
