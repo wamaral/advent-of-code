f588234ce6aee0818a508af8615606bf1096c48a
Wagner Amaral <wamaral@wamaral.org>
2021-12-01 17:00:49 +0100

    2021/1.1

--- 2021/src/Day1.hs
+++ 2021/src/Day1.hs
@@ -2,8 +2,11 @@ module Day1
   (day1part1, day1part2)
   where
 
+import           Common
+import           Data.List.Split
+
 day1part1 :: String -> String
-day1part1 _ = ""
+day1part1 = show . length . concat . splitWhen (uncurry (>)) . zipTail . readListOf intParser
 
 day1part2 :: String -> String
 day1part2 _ = ""
--- 2021/test/Day1Spec.hs
+++ 2021/test/Day1Spec.hs
@@ -6,13 +6,24 @@ import           Day1
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "199"
+  , "200"
+  , "208"
+  , "210"
+  , "200"
+  , "207"
+  , "240"
+  , "269"
+  , "260"
+  , "263"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day1part1 input `shouldBe` ""
+      day1part1 input `shouldBe` "7"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day1part2 input `shouldBe` ""
