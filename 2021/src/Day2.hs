1bce0599fe90fee6cc3a2ede027243621d75945e
Wagner Amaral <wamaral@wamaral.org>
2021-12-02 10:58:15 +0100

    2021/2.2

--- 2021/src/Day2.hs
+++ 2021/src/Day2.hs
@@ -3,7 +3,9 @@ module Day2
   where
 
 import           Common
+import           Control.Lens         ((^.))
 import           Linear.V2
+import           Linear.V3
 import           Linear.Vector
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
@@ -17,16 +19,21 @@ commandParser = choice
   , Up <$> (string "up " *> intParser)
   ]
 
-commandMovement :: Command -> V2 Int
-commandMovement (Forward n) = V2 n 0
-commandMovement (Down n)    = V2 0 n
-commandMovement (Up n)      = V2 0 (negate n)
+commandVector :: Command -> V2 Int
+commandVector (Forward n) = V2 n 0
+commandVector (Down n)    = V2 0 n
+commandVector (Up n)      = V2 0 (negate n)
+
+commandMove :: V3 Int -> Command -> V3 Int
+commandMove (V3 x y z) (Forward n) = V3 (x + n) (y + n * z) z
+commandMove pos (Down n)           = pos + V3 0 0 n
+commandMove pos (Up n)             = pos + V3 0 0 (negate n)
 
 multCoords :: V2 Int -> Int
 multCoords (V2 x y) = x * y
 
 day2part1 :: String -> String
-day2part1 = show . multCoords . sumV . map commandMovement . readListOf commandParser
+day2part1 = show . multCoords . sumV . map commandVector . readListOf commandParser
 
 day2part2 :: String -> String
-day2part2 _ = ""
+day2part2 = show . multCoords . (^. _xy) . foldl commandMove (V3 0 0 0) . readListOf commandParser
--- 2021/test/Day2Spec.hs
+++ 2021/test/Day2Spec.hs
@@ -22,4 +22,4 @@ spec = do
       day2part1 input `shouldBe` "150"
   describe "part 2" $ do
     it "runs provided examples" $ do
-      day2part2 input `shouldBe` ""
+      day2part2 input `shouldBe` "900"
