a67d13fc6de063a3b9493941ace6a103f63c85cc
Wagner Amaral <wamaral@wamaral.org>
2021-12-02 10:37:25 +0100

    2021/2.1

--- 2021/src/Day2.hs
+++ 2021/src/Day2.hs
@@ -2,8 +2,31 @@ module Day2
   (day2part1, day2part2)
   where
 
+import           Common
+import           Linear.V2
+import           Linear.Vector
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Command = Forward Int | Down Int | Up Int deriving Show
+
+commandParser :: Parser Command
+commandParser = choice
+  [ Forward <$> (string "forward " *> intParser)
+  , Down <$> (string "down " *> intParser)
+  , Up <$> (string "up " *> intParser)
+  ]
+
+commandMovement :: Command -> V2 Int
+commandMovement (Forward n) = V2 n 0
+commandMovement (Down n)    = V2 0 n
+commandMovement (Up n)      = V2 0 (negate n)
+
+multCoords :: V2 Int -> Int
+multCoords (V2 x y) = x * y
+
 day2part1 :: String -> String
-day2part1 _ = ""
+day2part1 = show . multCoords . sumV . map commandMovement . readListOf commandParser
 
 day2part2 :: String -> String
 day2part2 _ = ""
--- 2021/test/Day2Spec.hs
+++ 2021/test/Day2Spec.hs
@@ -6,13 +6,20 @@ import           Day2
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "forward 5"
+  , "down 5"
+  , "forward 8"
+  , "up 3"
+  , "down 8"
+  , "forward 2"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day2part1 input `shouldBe` ""
+      day2part1 input `shouldBe` "150"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day2part2 input `shouldBe` ""
