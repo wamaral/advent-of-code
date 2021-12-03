b0dc8fc4a4b2f1d23b9bb5e27f1c797a29b00ab0
Wagner Amaral <wamaral@wamaral.org>
2021-12-03 16:52:56 +0100

    2021/3.1

--- 2021/src/Day3.hs
+++ 2021/src/Day3.hs
@@ -2,8 +2,32 @@ module Day3
   (day3part1, day3part2)
   where
 
+import           Common
+import           Control.Applicative
+import           Data.Char
+import           Data.Function
+import           Data.List
+import           Safe
+import           Text.Megaparsec.Char
+
+bitstringToInt :: String -> Int
+bitstringToInt = sum . zipWith fromBit [0 .. ] . reverse . map digitToInt
+  where
+    fromBit :: Int -> Int -> Int
+    fromBit index bitVal = 2 ^ index * bitVal
+
+reportParser :: Parser String
+reportParser = many binDigitChar
+
+majority :: String -> Char
+majority = headDef '0' . maximumBy (compare `on` length) . group . sort
+
+flipBits :: String -> String
+flipBits = map (\x -> if x == '1' then '0' else '1')
+
 day3part1 :: String -> String
-day3part1 _ = ""
+day3part1 = show . uncurry (*) . bothValues . map majority . transpose . readListOf reportParser
+  where bothValues x = (bitstringToInt x, bitstringToInt $ flipBits x)
 
 day3part2 :: String -> String
 day3part2 _ = ""
--- 2021/test/Day3Spec.hs
+++ 2021/test/Day3Spec.hs
@@ -6,13 +6,26 @@ import           Day3
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "00100"
+  , "11110"
+  , "10110"
+  , "10111"
+  , "10101"
+  , "01111"
+  , "00111"
+  , "11100"
+  , "10000"
+  , "11001"
+  , "00010"
+  , "01010"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day3part1 input `shouldBe` ""
+      day3part1 input `shouldBe` "198"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day3part2 input `shouldBe` ""
