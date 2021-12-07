6c01ce35a7ac77d09dcc7e8a275850dbd31b8d16
Wagner Amaral <wamaral@wamaral.org>
2021-12-07 04:10:12 +0100

    2021/6.1

--- 2021/src/Day6.hs
+++ 2021/src/Day6.hs
@@ -2,8 +2,35 @@ module Day6
   (day6part1, day6part2)
   where
 
+import           Common
+import           Data.Function
+import qualified Data.IntMap          as M
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Aquarium = M.IntMap Integer
+
+fishParser :: Parser [Int]
+fishParser = intParser `sepBy` char ',' <* optional newline
+
+parseInput :: String -> [Int]
+parseInput = fromMaybe [] . parseMaybe fishParser
+
+startingFish :: [Int] -> Aquarium
+startingFish = M.fromListWith (+) . flip zip (repeat 1)
+
+simulate :: Aquarium -> Aquarium
+simulate old = new
+  & M.insertWith (+) 6 procreating
+  & M.insertWith (+) 8 procreating
+  & M.delete (-1)
+  where
+    new = M.mapKeys pred old
+    procreating = M.lookup (-1) new & fromMaybe 0
+
 day6part1 :: String -> String
-day6part1 _ = ""
+day6part1 = show . sum . M.elems . last . take 81 . iterate simulate . startingFish . parseInput
 
 day6part2 :: String -> String
 day6part2 _ = ""
--- 2021/test/Day6Spec.hs
+++ 2021/test/Day6Spec.hs
@@ -6,13 +6,13 @@ import           Day6
 import           Test.Hspec
 
 input :: String
-input = ""
+input = "3,4,3,1,2"
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day6part1 input `shouldBe` ""
+      day6part1 input `shouldBe` "5934"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day6part2 input `shouldBe` ""
