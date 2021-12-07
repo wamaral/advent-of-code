2d13bfbd6e537033d9d882cdec1f132069b5db21
Wagner Amaral <wamaral@wamaral.org>
2021-12-07 03:20:05 +0100

    2021/5.1

--- 2021/src/Day5.hs
+++ 2021/src/Day5.hs
@@ -2,8 +2,53 @@ module Day5
   (day5part1, day5part2)
   where
 
+import           Common
+import           Control.Lens
+import qualified Data.Map             as M
+import           Linear.V2
+import           Linear.Vector
+import           Text.Megaparsec.Char
+
+type Point = V2 Int
+type Line = [Point]
+
+parsePoints :: Parser (Point, Point)
+parsePoints = do
+  x1 <- intParser
+  _ <- char ','
+  y1 <- intParser
+  _ <- string " -> "
+  x2 <- intParser
+  _ <- char ','
+  y2 <- intParser
+  return (V2 x1 y1, V2 x2 y2)
+
+interpolate :: (Enum a, Eq a, Ord a) => a -> a -> [a]
+interpolate a b
+  | a < b = [a..b]
+  | a > b = [a,(pred a)..b]
+  | otherwise = []
+
+makeLine :: (Point, Point) -> Line
+makeLine (V2 x1 y1, V2 x2 y2)
+  | x1 == x2 = map (V2 x1) (interpolate y1 y2)
+  | y1 == y2 = map (`V2` y1) (interpolate x1 x2)
+  | otherwise = zipWith V2 (interpolate x1 x2) (interpolate y1 y2)
+
+isStraight :: Line -> Bool
+isStraight ps = 0 `elem` [diff ^._x, diff ^._y]
+  where diff = head ps ^-^ last ps
+
 day5part1 :: String -> String
-day5part1 _ = ""
+day5part1 = show
+  . M.size
+  . M.filter (> 1)
+  . M.fromListWith (+)
+  . flip zip (repeat 1 :: [Int])
+  . concat
+  . filter isStraight
+  . map makeLine
+  . readListOf parsePoints
 
 day5part2 :: String -> String
 day5part2 _ = ""
--- 2021/test/Day5Spec.hs
+++ 2021/test/Day5Spec.hs
@@ -6,13 +6,24 @@ import           Day5
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "0,9 -> 5,9"
+  , "8,0 -> 0,8"
+  , "9,4 -> 3,4"
+  , "2,2 -> 2,1"
+  , "7,0 -> 7,4"
+  , "6,4 -> 2,0"
+  , "0,9 -> 2,9"
+  , "3,4 -> 1,4"
+  , "0,0 -> 8,8"
+  , "5,5 -> 8,2"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day5part1 input `shouldBe` ""
+      day5part1 input `shouldBe` "5"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day5part2 input `shouldBe` ""
