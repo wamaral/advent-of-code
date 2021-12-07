7aa5827e8375f834e946458e81349d849873fdf4
Wagner Amaral <wamaral@wamaral.org>
2021-12-07 19:31:07 +0100

    2021/7.bench

--- 2021/bench/Main.hs
+++ 2021/bench/Main.hs
@@ -56,10 +56,10 @@ main = defaultMain
       [ bench "part 1" $ nf day6part1 input
       , bench "part 2" $ nf day6part2 input
       ]
-  -- , env (readInput 7) $ \input -> bgroup "Day 7"
-  --     [ bench "part 1" $ nf day7part1 input
-  --     , bench "part 2" $ nf day7part2 input
-  --     ]
+  , env (readInput 7) $ \input -> bgroup "Day 7"
+      [ bench "part 1" $ nf day7part1 input
+      , bench "part 2" $ nf day7part2 input
+      ]
   -- , env (readInput 8) $ \input -> bgroup "Day 8"
   --     [ bench "part 1" $ nf day8part1 input
   --     , bench "part 2" $ nf day8part2 input
