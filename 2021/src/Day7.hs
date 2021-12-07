4b3a41eed52e5ca19cc8a879610d071991eaa9a7
Wagner Amaral <wamaral@wamaral.org>
2021-12-07 19:28:41 +0100

    2021/7.1

--- 2021/src/Day7.hs
+++ 2021/src/Day7.hs
@@ -2,8 +2,36 @@ module Day7
   (day7part1, day7part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+crabParser :: Parser [Int]
+crabParser = intParser `sepBy` char ',' <* optional newline
+
+parseInput :: String -> [Int]
+parseInput = fromMaybe [] . parseMaybe crabParser
+
+-- From https://hackage.haskell.org/package/dsp-0.2.5.1/docs/src/Numeric.Statistics.Median.html#median
+-- Converted to return Integral instead of Fractional
+median :: (Ord a, Integral a) => [a] -> a
+median x =
+   if odd n
+     then sort x !! (n `div` 2)
+     else ((sort x !! (n `div` 2 - 1)) + (sort x !! (n `div` 2))) `div` 2
+    where n = length x
+
 day7part1 :: String -> String
-day7part1 _ = ""
+day7part1 input = crabs
+  & map (\x -> abs $ x - pos)
+  & sum
+  & show
+  where
+    crabs = parseInput input
+    pos = median crabs
 
 day7part2 :: String -> String
 day7part2 _ = ""
--- 2021/test/Day7Spec.hs
+++ 2021/test/Day7Spec.hs
@@ -6,13 +6,13 @@ import           Day7
 import           Test.Hspec
 
 input :: String
-input = ""
+input = "16,1,2,0,4,2,7,1,2,14"
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day7part1 input `shouldBe` ""
+      day7part1 input `shouldBe` "37"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day7part2 input `shouldBe` ""
