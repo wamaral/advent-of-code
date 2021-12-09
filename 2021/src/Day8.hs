18531ede0e8045e8637ee4caa875f755170fc60f
Wagner Amaral <wamaral@wamaral.org>
2021-12-09 02:37:34 +0100

    2021/8.1

--- 2021/src/Day8.hs
+++ 2021/src/Day8.hs
@@ -1,9 +1,27 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day8
   (day8part1, day8part2)
   where
 
+import           Common
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Display = Display { signals :: [String], output :: [String] } deriving Show
+
+wireParser :: Parser Char
+wireParser = oneOf "abcdefg"
+
+displayParser :: Parser Display
+displayParser = do
+  signals <- someTill (some wireParser <* hspace) $ string "| "
+  output <- some wireParser `sepBy` hspace
+  return Display{..}
+
 day8part1 :: String -> String
-day8part1 _ = ""
+day8part1 = show . length . filter is1478 . concatMap output . readListOf displayParser
+  where is1478 n = length n `elem` [2, 3, 4, 7]
 
 day8part2 :: String -> String
 day8part2 _ = ""
--- 2021/test/Day8Spec.hs
+++ 2021/test/Day8Spec.hs
@@ -6,13 +6,24 @@ import           Day8
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+  , "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
+  , "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"
+  , "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+  , "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
+  , "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
+  , "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
+  , "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
+  , "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
+  , "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day8part1 input `shouldBe` ""
+      day8part1 input `shouldBe` "26"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day8part2 input `shouldBe` ""
