26e287e2b45b779643f85795f596a6e532e03403
Wagner Amaral <wamaral@wamaral.org>
2021-12-10 18:56:34 +0100

    2021/9.1

--- 2021/src/Day9.hs
+++ 2021/src/Day9.hs
@@ -2,8 +2,34 @@ module Day9
   (day9part1, day9part2)
   where
 
+import           Common
+import           Data.Function
+import qualified Data.Map             as M
+import           Data.Maybe
+import           Linear.V2
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Cave = M.Map (V2 Int) Int
+
+caveParser :: Parser Cave
+caveParser = listOfListsToV2Map <$> someTill (someTill charAsInt newline) eof
+  where charAsInt = stringToInt0 . (:[]) <$> digitChar
+
+parseInput :: String -> Cave
+parseInput = fromMaybe M.empty . parseMaybe caveParser
+
+adjacents :: V2 Int -> [V2 Int]
+adjacents p = map (+ p) [V2 0 1, V2 1 0, V2 0 (-1), V2 (-1) 0]
+
+isLowPoint :: Cave -> V2 Int -> Int -> Bool
+isLowPoint cave p h = all (> h) $ mapMaybe (`M.lookup` cave) (adjacents p)
+
 day9part1 :: String -> String
-day9part1 _ = ""
+day9part1 input = M.filterWithKey (isLowPoint cave) cave
+  & M.foldr (\x acc -> acc + succ x) 0
+  & show
+  where cave = parseInput input
 
 day9part2 :: String -> String
 day9part2 _ = ""
--- 2021/test/Day9Spec.hs
+++ 2021/test/Day9Spec.hs
@@ -6,13 +6,19 @@ import           Day9
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "2199943210"
+  , "3987894921"
+  , "9856789892"
+  , "8767896789"
+  , "9899965678"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day9part1 input `shouldBe` ""
+      day9part1 input `shouldBe` "15"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day9part2 input `shouldBe` ""
