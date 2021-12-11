6399ad956b905897a5d2f1add4dd0d7e6247c3f4
Wagner Amaral <wamaral@wamaral.org>
2021-12-11 15:14:37 +0100

    2021/11.1

--- 2021/src/Day11.hs
+++ 2021/src/Day11.hs
@@ -2,8 +2,53 @@ module Day11
   (day11part1, day11part2)
   where
 
+import           Common
+import           Data.Bifunctor
+import           Data.Function
+import qualified Data.Map             as M
+import           Data.Maybe
+import           Linear.V2
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Cave = M.Map (V2 Int) Int
+
+caveParser :: Parser Cave
+caveParser = listOfListsToV2Map <$> someTill (someTill charAsInt newline) eof
+  where charAsInt = stringToInt0 . (:[]) <$> digitChar
+
+parseInput :: String -> Cave
+parseInput = fromMaybe M.empty . parseMaybe caveParser
+
+adjacentDirections :: [V2 Int]
+adjacentDirections = [V2 (-1) (-1), V2 0 (-1), V2 1 (-1),
+                      V2 (-1) 0,               V2 1 0,
+                      V2 (-1) 1,    V2 0 1,    V2 1 1]
+
+adjacents :: V2 Int -> [V2 Int]
+adjacents p = map (+ p) adjacentDirections
+
+step :: Cave -> (Cave, Int)
+step cave = go firstStep (flashes firstStep)
+  & resetCave
+  & (\a -> (a, M.filter (== 0) a & M.size))
+  where
+    firstStep = M.map succ cave
+    flashes c = M.filter (> 9) c & M.keys
+    resetCave = M.map (\x -> if x == (-1) then 0 else x)
+    burnFlashes = M.map (\x -> if x > 9 then -1 else x)
+    succ' n = if n == (-1) then n else succ n
+    go cv flashed = if null flashed then cv else do
+      let toInc = concatMap adjacents flashed & (++ flashed)
+      let newCave = foldr (M.adjust succ') (burnFlashes cv) toInc
+      go newCave (flashes newCave)
+
+steps :: Int -> Cave -> (Cave, Int)
+steps n cave = iterate go (cave, 0) !! max 0 n
+  where go (c, i) = step c & second (i +)
+
 day11part1 :: String -> String
-day11part1 _ = ""
+day11part1 = show . snd . steps 100 . parseInput
 
 day11part2 :: String -> String
 day11part2 _ = ""
--- 2021/test/Day11Spec.hs
+++ 2021/test/Day11Spec.hs
@@ -6,13 +6,24 @@ import           Day11
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "5483143223"
+  , "2745854711"
+  , "5264556173"
+  , "6141336146"
+  , "6357385478"
+  , "4167524645"
+  , "2176841721"
+  , "6882881134"
+  , "4846848554"
+  , "5283751526"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day11part1 input `shouldBe` ""
+      day11part1 input `shouldBe` "1656"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day11part2 input `shouldBe` ""
