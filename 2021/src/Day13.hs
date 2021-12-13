4647264cad04388e55334b41a25577dbacc56f1f
Wagner Amaral <wamaral@wamaral.org>
2021-12-13 19:23:36 +0100

    2021/13.1

--- 2021/src/Day13.hs
+++ 2021/src/Day13.hs
@@ -2,8 +2,58 @@ module Day13
   (day13part1, day13part2)
   where
 
+import           Common
+import           Control.Lens         hiding (Fold)
+import           Data.List
+import           Data.Maybe
+import qualified Data.Set             as S
+import           Linear.V2
+import           Linear.Vector
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Dot = V2 Int
+type Dots = S.Set Dot
+type Fold = V2 Int
+
+dotParser :: Parser Dot
+dotParser = do
+  x <- intParser
+  _ <- char ','
+  y <- intParser
+  return $ V2 x y
+
+foldParser :: Parser Fold
+foldParser = do
+  _ <- string "fold along "
+  xy <- oneOf "xy"
+  _ <- char '='
+  n <- intParser
+  return $ if xy == 'x' then V2 n 0 else V2 0 n
+
+inputParser :: Parser (Dots, [Fold])
+inputParser = do
+  dots <- some (dotParser <* optional newline)
+  _ <- newline
+  folds <- some (foldParser <* optional newline)
+  _ <- eof
+  return (S.fromList dots, folds)
+
+parseInput :: String -> (Dots, [Fold])
+parseInput = fromMaybe (S.empty, []) . parseMaybe inputParser
+
+foldDots :: Dots -> Fold -> Dots
+foldDots dots fold = S.map (\dot -> if toFold dot then dot - (diff dot & normalize & (* 2)) else dot) dots
+  where
+    toFold dot = dot ^._x >= fold ^._x && dot ^._y >= fold ^._y
+    diff dot = dot ^-^ fold
+    normalize dot = if fold ^._x == 0 then dot * V2 0 1 else dot * V2 1 0
+
 day13part1 :: String -> String
-day13part1 _ = ""
+day13part1 input = foldl' foldDots dots (take 1 folds)
+  & S.size
+  & show
+  where (dots, folds) = parseInput input
 
 day13part2 :: String -> String
 day13part2 _ = ""
--- 2021/test/Day13Spec.hs
+++ 2021/test/Day13Spec.hs
@@ -6,13 +6,35 @@ import           Day13
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "6,10"
+  , "0,14"
+  , "9,10"
+  , "0,3"
+  , "10,4"
+  , "4,11"
+  , "6,0"
+  , "6,12"
+  , "4,1"
+  , "0,13"
+  , "10,12"
+  , "3,4"
+  , "3,0"
+  , "8,4"
+  , "1,10"
+  , "2,14"
+  , "8,10"
+  , "9,0"
+  , ""
+  , "fold along y=7"
+  , "fold along x=5"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day13part1 input `shouldBe` ""
+      day13part1 input `shouldBe` "17"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day13part2 input `shouldBe` ""
