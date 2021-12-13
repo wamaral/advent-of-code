5da55b716e4c7fc624a8b5f13d16e42be324d9cc
Wagner Amaral <wamaral@wamaral.org>
2021-12-13 17:57:22 +0100

    2021/12.1

--- 2021/src/Day12.hs
+++ 2021/src/Day12.hs
@@ -2,8 +2,68 @@ module Day12
   (day12part1, day12part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import qualified Data.Map             as M
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data Cave = Start | End | Small String | Big String deriving (Eq, Ord, Show)
+
+type CaveMap = M.Map Cave [Cave]
+
+data CaveTree = Branch Cave [CaveTree] deriving Show
+
+caveParser :: Parser Cave
+caveParser = choice
+  [ Start <$ string "start"
+  , End <$ string "end"
+  , Small <$> some lowerChar
+  , Big <$> some upperChar
+  ]
+
+cavePathParser :: Parser (Cave, Cave)
+cavePathParser = do
+  cave1 <- caveParser
+  _ <- char '-'
+  cave2 <- caveParser
+  return (cave1, cave2)
+
+mkMap :: [(Cave, Cave)] -> CaveMap
+mkMap = foldl' (\m (a, b) -> M.insertWith (++) a (filter (not . isStart) [b]) m & M.insertWith (++) b (filter (not . isStart) [a])) M.empty
+
+mkTree :: CaveMap -> CaveTree
+mkTree caveMap = go [] Start
+  where
+    go :: [Cave] -> Cave -> CaveTree
+    go _ End = Branch End []
+    go visited cave = do
+      let toVisit = M.lookup cave caveMap & fromMaybe [] & filter (`notElem` visited)
+      let newVisited = if nonRevisitable cave then cave:visited else visited
+      Branch cave (map (go newVisited) toVisit)
+
+isStart :: Cave -> Bool
+isStart Start = True
+isStart _     = False
+
+nonRevisitable :: Cave -> Bool
+nonRevisitable (Small _) = True
+nonRevisitable _         = False
+
+allPaths :: CaveTree -> [[Cave]]
+allPaths caveTree = paths [] caveTree & map reverse
+  where
+    paths :: [Cave] -> CaveTree -> [[Cave]]
+    paths ps (Branch c [])       = [c:ps]
+    paths ps (Branch c subTrees) = concatMap (paths (c:ps)) subTrees
+
+isCompletePath :: [Cave] -> Bool
+isCompletePath = (== End) . last
+
 day12part1 :: String -> String
-day12part1 _ = ""
+day12part1 = show . length . filter isCompletePath . allPaths . mkTree . mkMap . readListOf cavePathParser
 
 day12part2 :: String -> String
 day12part2 _ = ""
--- 2021/test/Day12Spec.hs
+++ 2021/test/Day12Spec.hs
@@ -6,13 +6,59 @@ import           Day12
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "start-A"
+  , "start-b"
+  , "A-c"
+  , "A-b"
+  , "b-d"
+  , "A-end"
+  , "b-end"
+  ]
+
+input2 :: String
+input2 = unlines
+  [ "dc-end"
+  , "HN-start"
+  , "start-kj"
+  , "dc-start"
+  , "dc-HN"
+  , "LN-dc"
+  , "HN-end"
+  , "kj-sa"
+  , "kj-HN"
+  , "kj-dc"
+  ]
+
+input3 :: String
+input3 = unlines
+  [ "fs-end"
+  , "he-DX"
+  , "fs-he"
+  , "start-DX"
+  , "pj-DX"
+  , "end-zg"
+  , "zg-sl"
+  , "zg-pj"
+  , "pj-he"
+  , "RW-he"
+  , "fs-DX"
+  , "pj-RW"
+  , "zg-RW"
+  , "start-pj"
+  , "he-WI"
+  , "zg-he"
+  , "pj-fs"
+  , "start-RW"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day12part1 input `shouldBe` ""
+      day12part1 input `shouldBe` "10"
+      day12part1 input2 `shouldBe` "19"
+      day12part1 input3 `shouldBe` "226"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day12part2 input `shouldBe` ""
