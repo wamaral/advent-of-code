70a30200dbfcf57de7a9eaa018626947f2769d0a
Wagner Amaral <wamaral@wamaral.org>
2021-12-14 12:05:22 +0100

    2021/14.1

--- 2021/src/Day14.hs
+++ 2021/src/Day14.hs
@@ -2,8 +2,53 @@ module Day14
   (day14part1, day14part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import qualified Data.Map             as M
+import           Data.Maybe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Polymer = String
+type Rules = M.Map String Char
+
+ruleParser :: Parser (String, Char)
+ruleParser = do
+  pair <- some upperChar
+  _ <- string " -> "
+  c <- upperChar
+  return (pair, c)
+
+rulesParser :: Parser Rules
+rulesParser = M.fromList <$> some (ruleParser <* optional newline)
+
+inputParser :: Parser (Polymer, Rules)
+inputParser = do
+  polymer <- some (upperChar <* optional newline)
+  _ <- newline
+  rules <- rulesParser
+  _ <- eof
+  return (polymer, rules)
+
+parseInput :: String -> (Polymer, Rules)
+parseInput = fromMaybe ("", M.empty) . parseMaybe inputParser
+
+step :: Rules -> Polymer -> Polymer
+step rules p = zipTail p & concatMap applyRule & (head p :)
+  where applyRule (a,b) = maybe [b] (\c -> [c,b]) $ M.lookup [a,b] rules
+
 day14part1 :: String -> String
-day14part1 _ = ""
+day14part1 input = polymer
+  & iterate (step rules)
+  & take 11
+  & last
+  & sort
+  & group
+  & (\xs -> (maximumBy (compare `on` length) xs, minimumBy (compare `on` length) xs))
+  & (\(a,b) -> length a - length b)
+  & show
+  where (polymer, rules) = parseInput input
 
 day14part2 :: String -> String
 day14part2 _ = ""
--- 2021/test/Day14Spec.hs
+++ 2021/test/Day14Spec.hs
@@ -6,13 +6,32 @@ import           Day14
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "NNCB"
+  , ""
+  , "CH -> B"
+  , "HH -> N"
+  , "CB -> H"
+  , "NH -> C"
+  , "HB -> C"
+  , "HC -> B"
+  , "HN -> C"
+  , "NN -> C"
+  , "BH -> H"
+  , "NC -> B"
+  , "NB -> B"
+  , "BN -> B"
+  , "BB -> N"
+  , "BC -> B"
+  , "CC -> N"
+  , "CN -> C"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day14part1 input `shouldBe` ""
+      day14part1 input `shouldBe` "1588"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day14part2 input `shouldBe` ""
