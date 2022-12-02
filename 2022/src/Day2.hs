1c621e018abe3a849bb6dadb9290eafc165ae525
Wagner Amaral <wamaral@wamaral.org>
2022-12-02 12:14:12 +0100

    2022/2.1

--- 2022/src/Day2.hs
+++ 2022/src/Day2.hs
@@ -2,8 +2,38 @@ module Day2
   (day2part1, day2part2)
   where
 
+import           Common
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data RPS = Rock | Paper | Scissor deriving (Eq, Show)
+
+rpsParser :: Parser RPS
+rpsParser = choice
+  [ Rock <$ (char 'A' <|> char 'X')
+  , Paper <$ (char 'B' <|> char 'Y')
+  , Scissor <$ (char 'C' <|> char 'Z')
+  ]
+
+lineParser :: Parser (RPS, RPS)
+lineParser = (,) <$> (rpsParser <* hspace) <*> rpsParser
+
+shapeScore :: RPS -> Int
+shapeScore Rock    = 1
+shapeScore Paper   = 2
+shapeScore Scissor = 3
+
+outcomeScore :: (RPS, RPS) -> Int
+outcomeScore (Scissor, Rock)  = 6
+outcomeScore (Paper, Scissor) = 6
+outcomeScore (Rock, Paper)    = 6
+outcomeScore (a, b)           = if a == b then 3 else 0
+
+roundScore :: (RPS, RPS) -> Int
+roundScore l@(_, b) = outcomeScore l + shapeScore b
+
 day2part1 :: String -> String
-day2part1 _ = ""
+day2part1 = show . sum . map roundScore . readListOf lineParser
 
 day2part2 :: String -> String
 day2part2 _ = ""
--- 2022/test/Day2Spec.hs
+++ 2022/test/Day2Spec.hs
@@ -6,13 +6,17 @@ import           Day2
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "A Y"
+  , "B X"
+  , "C Z"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day2part1 input `shouldBe` ""
+      day2part1 input `shouldBe` "15"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day2part2 input `shouldBe` ""
