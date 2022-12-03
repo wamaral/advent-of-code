2750933726225dac1fff8cc5673d9bb340eaeda6
Wagner Amaral <wamaral@wamaral.org>
2022-12-03 14:13:29 +0100

    2022/3.1

--- 2022/src/Day3.hs
+++ 2022/src/Day3.hs
@@ -2,8 +2,27 @@ module Day3
   (day3part1, day3part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import           Safe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Sack = (String, String)
+
+sackParser :: Parser Sack
+sackParser = do
+  chars <- some letterChar <* optional newline
+  pure $ splitAt (length chars `div` 2) chars
+
+priority :: Char -> Int
+priority c = zip (['a' .. 'z'] ++ ['A'..'Z']) [1 ..]
+  & find (\(a,_) -> a == c)
+  & maybe 0 snd
+
 day3part1 :: String -> String
-day3part1 _ = ""
+day3part1 = show . sum . map (priority . headDef ' ' . uncurry intersect) . readListOf sackParser
 
 day3part2 :: String -> String
 day3part2 _ = ""
--- 2022/test/Day3Spec.hs
+++ 2022/test/Day3Spec.hs
@@ -6,13 +6,20 @@ import           Day3
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "vJrwpWtwJgWrhcsFMMfFFhFp"
+  , "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+  , "PmmdzqPrVvPwwTWBwg"
+  , "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+  , "ttgJtRGJQctTZtZT"
+  , "CrZsJsPPZsGzwwsLwLmpwMDw"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day3part1 input `shouldBe` ""
+      day3part1 input `shouldBe` "157"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day3part2 input `shouldBe` ""
