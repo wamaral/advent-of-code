94df8abc193d2e61c85db0c50c260382b8ede1ff
Wagner Amaral <wamaral@wamaral.org>
2022-12-06 17:23:47 +0100

    2022/6.1

--- 2022/src/Day6.hs
+++ 2022/src/Day6.hs
@@ -2,8 +2,18 @@ module Day6
   (day6part1, day6part2)
   where
 
+import           Data.Function
+import           Data.List
+import           Data.Maybe
+import qualified Data.Set      as S
+
 day6part1 :: String -> String
-day6part1 _ = ""
+day6part1 input = zipWith4 (\a b c d -> [a,b,c,d]) input (drop 1 input) (drop 2 input) (drop 3 input)
+  & findIndex allDistinct
+  & fmap (+ 4)
+  & fromMaybe 0
+  & show
+  where allDistinct = (== 4) . S.size . S.fromList
 
 day6part2 :: String -> String
 day6part2 _ = ""
--- 2022/test/Day6Spec.hs
+++ 2022/test/Day6Spec.hs
@@ -6,13 +6,25 @@ import           Day6
 import           Test.Hspec
 
 input :: String
-input = ""
+input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+input2 :: String
+input2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+input3 :: String
+input3 = "nppdvjthqldpwncqszvftbrmjlhg"
+input4 :: String
+input4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+input5 :: String
+input5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day6part1 input `shouldBe` ""
+      day6part1 input `shouldBe` "7"
+      day6part1 input2 `shouldBe` "5"
+      day6part1 input3 `shouldBe` "6"
+      day6part1 input4 `shouldBe` "10"
+      day6part1 input5 `shouldBe` "11"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day6part2 input `shouldBe` ""
