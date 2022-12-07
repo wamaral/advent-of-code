c20ba5c8fc712c37840feda583917e686de385b5
Wagner Amaral <wamaral@wamaral.org>
2022-12-07 14:37:28 +0100

    2022/5.1

--- 2022/src/Day5.hs
+++ 2022/src/Day5.hs
@@ -1,9 +1,82 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day5
   (day5part1, day5part2)
   where
 
+import           Common
+import           Data.Function
+import qualified Data.IntMap.Strict   as M
+import           Data.List
+import           Data.Maybe
+import qualified Data.Stack           as S
+import           Safe
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type CrateLine = [Maybe Char]
+type CrateStack = S.Stack Char
+type Crates = M.IntMap CrateStack
+
+data Command = Command { cnt :: Int, src :: Int, dst :: Int } deriving Show
+
+crateParser :: Parser (Maybe Char)
+crateParser = choice
+  [ Just <$> (char '[' *> (upperChar <* char ']'))
+  , Nothing <$ string "   "
+  ]
+
+crateLineParser :: Parser CrateLine
+crateLineParser = some (crateParser <* optional (char ' '))
+
+crateLinesToCrates :: [CrateLine] -> Crates
+crateLinesToCrates input = transpose input
+  & map (stackEm . reverse . catMaybes)
+  & zip [1..]
+  & M.fromDistinctAscList
+  where stackEm = foldl' S.stackPush S.stackNew
+
+commandParser :: Parser Command
+commandParser = do
+  _ <- string "move "
+  cnt <- intParser
+  _ <- string " from "
+  src <- intParser
+  _ <- string " to "
+  dst <- intParser
+  _ <- newline
+  pure Command{..}
+
+inputParser :: Parser (Crates, [Command])
+inputParser = do
+  ls <- crateLinesToCrates <$> some (crateLineParser <* newline)
+  _ <- some (digitChar <|> char ' ') <* newline
+  _ <- newline
+  cm <- someTill (commandParser <* optional newline) eof
+  pure (ls, cm)
+
+parseInput :: String -> (Crates, [Command])
+parseInput = fromMaybe (M.empty, []) . parseMaybe inputParser
+
+moveOne :: Int -> Int -> Crates -> Crates
+moveOne from to crates = do
+  let fromCrate = (M.!) crates from
+  let toCrate = (M.!) crates to
+  case S.stackPop fromCrate of
+    Just (newFrom, popped) -> do
+      let newTo = S.stackPush toCrate popped
+      M.insert to newTo $ M.insert from newFrom crates
+    Nothing -> crates
+
+runCommand :: Crates -> Command -> Crates
+runCommand crates (Command n from to) = atDef crates (iterate (moveOne from to) crates) n
+
 day5part1 :: String -> String
-day5part1 _ = ""
+day5part1 input = foldl' runCommand crates commands
+  & M.elems
+  & mapM S.stackPeek
+  & fromMaybe ""
+  where (crates, commands) = parseInput input
 
 day5part2 :: String -> String
 day5part2 _ = ""
--- 2022/test/Day5Spec.hs
+++ 2022/test/Day5Spec.hs
@@ -6,13 +6,23 @@ import           Day5
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "    [D]    "
+  , "[N] [C]    "
+  , "[Z] [M] [P]"
+  , " 1   2   3 "
+  , ""
+  , "move 1 from 2 to 1"
+  , "move 3 from 1 to 3"
+  , "move 2 from 2 to 1"
+  , "move 1 from 1 to 2"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day5part1 input `shouldBe` ""
+      day5part1 input `shouldBe` "CMZ"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day5part2 input `shouldBe` ""
