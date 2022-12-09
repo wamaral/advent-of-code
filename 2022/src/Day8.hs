860c3afb0de212f173906d4f2773812a51e871e0
Wagner Amaral <wamaral@wamaral.org>
2022-12-09 12:04:24 +0100

    2022/8.2

--- 2022/src/Day8.hs
+++ 2022/src/Day8.hs
@@ -7,6 +7,7 @@ import           Control.Lens
 import qualified Data.Map.Strict as M
 import           Data.Maybe
 import           Linear.V2
+import           Safe
 import           Text.Megaparsec
 
 type Tree = V2 Int
@@ -29,19 +30,40 @@ lineOfSight boundary tree = map (filter (/= tree)) [left, right, up, down]
     up = map (V2 (tree ^._x)) [0..(tree ^._y)]
     down = map (V2 (tree ^._x)) [(tree ^._y)..(boundary ^._y)]
 
-isVisible :: Forest -> Tree -> Bool
-isVisible forest tree = lineOfSight (forestBoundary forest) tree
+isVisibleFromOutside :: Forest -> Tree -> Bool
+isVisibleFromOutside forest tree = lineOfSight (forestBoundary forest) tree
   & map (map (forest M.!))
   & any (all (< treeSize))
   where treeSize = (M.!) forest tree
 
+canSeeTrees :: Forest -> Tree -> Int
+canSeeTrees forest tree = canSee up * canSee down * canSee left * canSee right
+  where
+    treeX = tree ^._x
+    treeY = tree ^._y
+    boundary = forestBoundary forest
+    boundaryX = boundary ^._x
+    boundaryY = boundary ^._y
+    left = map (`V2` treeY) [treeX,treeX-1..0] & tail
+    right = map (`V2` treeY) [treeX..boundaryX] & tail
+    up = map (V2 treeX) [treeY,treeY-1..0] & tail
+    down = map (V2 treeX) [treeY..boundaryY] & tail
+    treeSize = (M.!) forest
+    thisTreeSize = treeSize tree
+    canSee direction = (\(a,b) -> length a + (if headDef 0 b >= thisTreeSize then 1 else 0)) $ span (< thisTreeSize) (map treeSize direction)
+
 day8part1 :: String -> String
 day8part1 input = forest
   & M.keys
-  & filter (isVisible forest)
+  & filter (isVisibleFromOutside forest)
   & length
   & show
   where forest = parseInput input
 
 day8part2 :: String -> String
-day8part2 _ = ""
+day8part2 input = forest
+  & M.keys
+  & map (canSeeTrees forest)
+  & maximum
+  & show
+  where forest = parseInput input
--- 2022/test/Day8Spec.hs
+++ 2022/test/Day8Spec.hs
@@ -21,4 +21,4 @@ spec = do
       day8part1 input `shouldBe` "21"
   describe "part 2" $ do
     it "runs provided examples" $ do
-      day8part2 input `shouldBe` ""
+      day8part2 input `shouldBe` "8"
