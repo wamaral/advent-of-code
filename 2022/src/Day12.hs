cc25834b1fa9006edc4f69ace5d7cf876aae2058
Wagner Amaral <wamaral@wamaral.org>
2022-12-16 15:01:06 +0100

    2022/12.1

--- 2022/src/Day12.hs
+++ 2022/src/Day12.hs
@@ -1,9 +1,74 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day12
   (day12part1, day12part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List
+import qualified Data.Map.Strict      as M
+import           Data.Maybe
+import qualified Data.Set             as S
+import           Data.Tuple
+import           Linear.V2
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+type Position = V2 Int
+type Elevation = Int
+type TerrainMap = M.Map Position Elevation
+type TerrainPaths = M.Map Position [Position]
+data Terrain = Terrain
+  { terrainMap     :: TerrainMap
+  , terrainPaths   :: TerrainPaths
+  , startPosition  :: Position
+  , targetPosition :: Position
+  } deriving Show
+
+elevationParser :: Parser Elevation
+elevationParser = toElevation <$> (lowerChar <|> char 'S' <|> char 'E')
+  where
+    toElevation 'S' = 0
+    toElevation 'E' = 27
+    toElevation x   = fromMaybe 99 $ lookup x $ zip ['a'..'z'] [1..]
+
+parseInput :: String -> Terrain
+parseInput input = do
+  let terrainMap = listOfListsToV2Map $ readListOf (many elevationParser) input
+  let terrainPaths = terrainMapToPaths terrainMap
+  let boundaries = M.filter (\e -> e == 0 || e == 27) terrainMap & M.assocs & map swap & sort & map snd
+  let startPosition = head boundaries
+  let targetPosition = last boundaries
+  Terrain{..}
+
+neighbours :: Position -> [Position]
+neighbours x = map (+ x) [V2 0 1, V2 0 (-1), V2 1 0, V2 (-1) 0]
+
+possibles :: TerrainMap -> Position -> TerrainMap
+possibles t p = neighbours p
+  & mapMaybe (switcheroo . (\p' -> (p', M.lookup p' t)))
+  & filter ((<= succ curElevation) . snd)
+  & M.fromList
+  where
+    curElevation = fromMaybe 99 $ M.lookup p t
+    switcheroo (a, Just b)  = Just (a, b)
+    switcheroo (_, Nothing) = Nothing
+
+terrainMapToPaths :: TerrainMap -> TerrainPaths
+terrainMapToPaths t = M.mapWithKey (\k _ -> M.keys $ possibles t k) t
+
+shortestCount :: Terrain -> S.Set Position -> S.Set Position -> Int -> Int
+shortestCount t toCheck visited cnt = do
+  let newToCheck = S.elems toCheck & mapMaybe (`M.lookup` terrainPaths t) & concat & filter (`S.notMember` visited) & S.fromList
+  if targetPosition t `elem` newToCheck
+    then cnt
+    else shortestCount t newToCheck (S.union visited toCheck) (cnt + 1)
+
+
 day12part1 :: String -> String
-day12part1 _ = ""
+day12part1 input = show $ shortestCount terrain (S.singleton (startPosition terrain)) S.empty 1
+  where terrain = parseInput input
 
 day12part2 :: String -> String
 day12part2 _ = ""
--- 2022/test/Day12Spec.hs
+++ 2022/test/Day12Spec.hs
@@ -6,13 +6,19 @@ import           Day12
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "Sabqponm"
+  , "abcryxxl"
+  , "accszExk"
+  , "acctuvwj"
+  , "abdefghi"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day12part1 input `shouldBe` ""
+      day12part1 input `shouldBe` "31"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day12part2 input `shouldBe` ""
