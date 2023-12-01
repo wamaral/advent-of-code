cb58106ec41c8bb7d731c6f97eb6658e3f82f5d7
Wagner Amaral <wamaral@wamaral.org>
2023-12-01 19:33:03 +0100

    2023/1.1

--- 2023/test/Day1Spec.hs
+++ 2023/test/Day1Spec.hs
@@ -6,13 +6,18 @@ import           Day1
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "1abc2"
+  , "pqr3stu8vwx"
+  , "a1b2c3d4e5f"
+  , "treb7uchet"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day1part1 input `shouldBe` ""
+      day1part1 input `shouldBe` "142"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day1part2 input `shouldBe` ""
