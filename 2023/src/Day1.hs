46659d50e38e4cb36910979f5ddfb7bc957a812f
Wagner Amaral <wamaral@wamaral.org>
2023-12-01 19:33:40 +0100

    2023/1.1.test

--- 2023/src/Day1.hs
+++ 2023/src/Day1.hs
@@ -2,8 +2,15 @@ module Day1
   (day1part1, day1part2)
   where
 
+import Common
+import Data.Char (isDigit)
+
+firstLast :: [a] -> [a]
+firstLast [] = []
+firstLast xs = [head xs, last xs]
+
 day1part1 :: String -> String
-day1part1 _ = ""
+day1part1 = show . sum . map (stringToInt0 . firstLast . filter isDigit) . lines
 
 day1part2 :: String -> String
 day1part2 _ = ""
