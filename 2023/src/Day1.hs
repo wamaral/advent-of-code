113f6b99afc5692389d21d442dc3a482e040e228
Wagner Amaral <wamaral@wamaral.org>
2023-12-02 10:26:12 +0100

    2023/1.2

--- 2023/src/Day1.hs
+++ 2023/src/Day1.hs
@@ -3,14 +3,26 @@ module Day1
   where
 
 import Common
-import Data.Char (isDigit)
+import Data.Char (intToDigit, isDigit)
+import Data.List (isPrefixOf, findIndex, tails)
+import Data.Maybe (fromMaybe)
 
 firstLast :: [a] -> [a]
 firstLast [] = []
 firstLast xs = [head xs, last xs]
 
+numbers :: [String]
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+textToNumber :: String -> Char
+textToNumber s = fromMaybe (head s) $ fmap intToDigit $ findIndex (`isPrefixOf` s) numbers
+
+getNumbers :: String -> String
+getNumbers [] = []
+getNumbers xs = map textToNumber $ filter (not . null) $ tails xs
+
 day1part1 :: String -> String
 day1part1 = show . sum . map (stringToInt0 . firstLast . filter isDigit) . lines
 
 day1part2 :: String -> String
-day1part2 _ = ""
+day1part2 = show . sum . map (stringToInt0 . firstLast . filter isDigit . getNumbers) . lines
--- 2023/test/Day1Spec.hs
+++ 2023/test/Day1Spec.hs
@@ -13,6 +13,17 @@ input = unlines
   , "treb7uchet"
   ]
 
+input2 :: String
+input2 = unlines
+  [ "two1nine"
+  , "eightwothree"
+  , "abcone2threexyz"
+  , "xtwone3four"
+  , "4nineeightseven2"
+  , "zoneight234"
+  , "7pqrstsixteen"
+  ]
+
 spec :: Spec
 spec = do
   describe "part 1" $ do
@@ -20,4 +31,5 @@ spec = do
       day1part1 input `shouldBe` "142"
   describe "part 2" $ do
     it "runs provided examples" $ do
-      day1part2 input `shouldBe` ""
+      day1part2 input `shouldBe` "142"
+      day1part2 input2 `shouldBe` "281"
