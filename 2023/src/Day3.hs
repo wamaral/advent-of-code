ed0ffaf78da43794bbc4e3d70ac6846af3344c60
Wagner Amaral <wamaral@wamaral.org>
2023-12-03 16:04:07 +0100

    2023/3.1

--- 2023/src/Day3.hs
+++ 2023/src/Day3.hs
@@ -2,8 +2,87 @@ module Day3
   (day3part1, day3part2)
   where
 
+import           Common
+import           Data.Function
+import           Data.List (nub)
+import qualified Data.Map.Strict as M
+import           Data.Maybe
+import           Linear.V2
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+data EnginePart = Digit Int | Symbol Char | Empty deriving Eq
+type Position = V2 Int
+type Engine = M.Map Position EnginePart
+
+instance Show EnginePart where
+  show (Digit i) = show i
+  show (Symbol s) = show s
+  show Empty = "Empty"
+
+isDigit :: EnginePart -> Bool
+isDigit (Digit _) = True
+isDigit _ = False
+
+isSymbol :: EnginePart -> Bool
+isSymbol (Symbol _) = True
+isSymbol _ = False
+
+partParser :: Parser EnginePart
+partParser = choice
+  [ Digit <$> singleDigitParser
+  , Empty <$ (char '.')
+  , Symbol <$> printChar
+  ]
+
+engineParser :: Parser Engine
+engineParser = listOfListsToV2Map <$> linesParser (some partParser)
+
+parseInput :: String -> Engine
+parseInput = fromMaybe M.empty . parseMaybe engineParser
+
+neighbourhood :: [Position]
+neighbourhood = [V2 (-1) (-1), V2 0 (-1), V2 1 (-1),
+                 V2 (-1) 0,    V2 0 0,    V2 1 0,
+                 V2 (-1) 1,    V2 0 1,    V2 1 1]
+
+neighbours :: Position -> [Position]
+neighbours p = map (+ p) neighbourhood
+
+neighboursWithDigits :: Engine -> Position -> [Position]
+neighboursWithDigits engine p = neighbours p
+  & filter (\pos -> maybe False isDigit $ M.lookup pos engine)
+
+digitStart :: Engine -> Position -> Position
+digitStart engine (V2 x y) = coords
+  & takeWhile (\p -> maybe False isDigit $ M.lookup p engine)
+  & reverse
+  & head
+  where
+    xs = reverse [0..x]
+    coords = map (`V2` y) xs
+
+symbolPositions :: Engine -> [Position]
+symbolPositions = M.keys . M.filter isSymbol
+
+numberAt :: Engine -> Position -> Int
+numberAt engine (V2 x y) = map (`V2` y) [x..]
+  & takeWhile (\p -> maybe False isDigit $ M.lookup p engine)
+  & map (\p -> M.lookup p engine)
+  & catMaybes
+  & concatMap show
+  & stringToInt0
+
 day3part1 :: String -> String
-day3part1 _ = ""
+day3part1 input = engine
+  & symbolPositions
+  & concatMap (neighboursWithDigits engine)
+  & map (digitStart engine)
+  & nub
+  & map (numberAt engine)
+  & sum
+  & show
+  where engine = parseInput input
 
 day3part2 :: String -> String
 day3part2 _ = ""
--- 2023/test/Day3Spec.hs
+++ 2023/test/Day3Spec.hs
@@ -6,13 +6,24 @@ import           Day3
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "467..114.."
+  , "...*......"
+  , "..35..633."
+  , "......#..."
+  , "617*......"
+  , ".....+.58."
+  , "..592....."
+  , "......755."
+  , "...$.*...."
+  , ".664.598.."
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day3part1 input `shouldBe` ""
+      day3part1 input `shouldBe` "4361"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day3part2 input `shouldBe` ""
