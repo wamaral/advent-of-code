81a57d0e75be50ab2115e6d286214825d29c6510
Wagner Amaral <wamaral@wamaral.org>
2023-12-05 19:50:42 +0100

    2023/4.2

--- 2023/src/Day4.hs
+++ 2023/src/Day4.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections   #-}
 
 module Day4
   (day4part1, day4part2)
@@ -7,6 +8,7 @@ module Day4
 import           Common
 import           Data.Function
 import qualified Data.IntSet as S
+import qualified Data.IntMap.Strict as M
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
@@ -15,6 +17,11 @@ data Card = Card
   , winningNumbers :: S.IntSet
   , cardNumbers :: S.IntSet
   } deriving Show
+
+type CardPile = M.IntMap Int
+
+newPile :: Int -> CardPile
+newPile size = M.fromDistinctAscList $ fmap (,1) [1..size]
 
 cardParser :: Parser Card
 cardParser = do
@@ -31,12 +38,30 @@ cardParser = do
   pure Card{..}
 
 cardValue :: Card -> Int
-cardValue card = S.intersection (winningNumbers card) (cardNumbers card)
-  & S.size
+cardValue card = cardValue2 card
   & (\n -> if n == 0 then 0 else 2 ^ (n - 1))
+
+cardValue2 :: Card -> Int
+cardValue2 card = S.intersection (winningNumbers card) (cardNumbers card)
+  & S.size
+
+checkCard :: CardPile -> Card -> CardPile
+checkCard pile card = map (+ cardId card) [0..value]
+  & tail
+  & foldr (M.adjust (+ cardsNow)) pile
+  where
+    value = cardValue2 card
+    cardsNow = M.findWithDefault 0 (cardId card) pile
 
 day4part1 :: String -> String
 day4part1 = show . sum . map cardValue . readListOf cardParser
 
 day4part2 :: String -> String
-day4part2 _ = ""
+day4part2 input = cards
+  & foldl checkCard pile
+  & M.elems
+  & sum
+  & show
+  where
+    cards = readListOf cardParser input
+    pile = newPile $ length cards
