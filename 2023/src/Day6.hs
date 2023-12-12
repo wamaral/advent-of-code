889d219fad12c207f341d0e46b7c2c51d93cd7df
Wagner Amaral <wamaral@wamaral.org>
2023-12-12 19:04:10 +0100

    2023/6.1

--- 2023/src/Day6.hs
+++ 2023/src/Day6.hs
@@ -2,8 +2,39 @@ module Day6
   (day6part1, day6part2)
   where
 
+import Common
+import Data.Function
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+data Race = Race { time :: Int, distance :: Int } deriving Show
+
+inputParser :: Parser [Race]
+inputParser = do
+  _ <- string "Time:"
+  _ <- hspace
+  times <- many (intParser <* optional hspace)
+  _ <- newline
+  _ <- string "Distance:"
+  _ <- hspace
+  distances <- many (intParser <* optional hspace)
+  _ <- newline
+  _ <- eof
+  pure $ zipWith Race times distances
+
+parseInput :: String -> [Race]
+parseInput = fromMaybe [] . parseMaybe inputParser
+
+-- I mean, I'm sure there's a formula, but this works... For now :)
+runRace :: Race -> Int
+runRace (Race t d) = [1..t]
+  & map (\n -> n * (t - n))
+  & filter (> d)
+  & length
+
 day6part1 :: String -> String
-day6part1 _ = ""
+day6part1 = show . product . map runRace . parseInput
 
 day6part2 :: String -> String
 day6part2 _ = ""
--- 2023/test/Day6Spec.hs
+++ 2023/test/Day6Spec.hs
@@ -6,13 +6,16 @@ import           Day6
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "Time:      7  15   30"
+  , "Distance:  9  40  200"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day6part1 input `shouldBe` ""
+      day6part1 input `shouldBe` "288"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day6part2 input `shouldBe` ""
