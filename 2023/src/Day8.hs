30512341b642b00139fe7bcdaf4f808b4d507706
Wagner Amaral <wamaral@wamaral.org>
2023-12-13 19:01:34 +0100

    2023/8.1

--- 2023/src/Day8.hs
+++ 2023/src/Day8.hs
@@ -1,9 +1,73 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day8
   (day8part1, day8part2)
   where
 
+import Common
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+data Instruction = L | R deriving (Eq, Show)
+type Position = String
+type PositionMap = M.Map Position (Position, Position)
+data Input = Input { instr :: [Instruction], pos :: PositionMap } deriving Show
+data MapState = MapState { instructions :: [Instruction], positions :: PositionMap, currentPosition :: Position, steps :: Int }
+
+instance Show MapState where
+  show s =  "@" ++ currentPosition s ++ " #" ++ show (steps s) ++ " :: " ++ show (take 10 $ instructions s)
+
+instructionParser :: Parser Instruction
+instructionParser = choice
+  [ L <$ char 'L'
+  , R <$ char 'R'
+  ]
+
+positionParser :: Parser (Position, (Position, Position))
+positionParser = do
+  src <- count 3 upperChar
+  _ <- string " = ("
+  l <- count 3 upperChar
+  _ <- string ", "
+  r <- count 3 upperChar
+  _ <- string ")"
+  pure (src, (l, r))
+
+inputParser :: Parser Input
+inputParser = do
+  instr <- many instructionParser
+  _ <- newline
+  _ <- newline
+  pos <- M.fromList <$> many (positionParser <* optional newline)
+  pure Input{..}
+
+parseInput :: String -> Input
+parseInput = fromMaybe Input{instr = [], pos = M.empty} . parseMaybe inputParser
+
+startingState :: Position -> Input -> MapState
+startingState startingPos input = MapState
+  { instructions = cycle (instr input)
+  , positions   = pos input
+  , currentPosition = startingPos
+  , steps = 0
+  }
+
+walk :: MapState -> MapState
+walk state = do
+  let instruction = head $ instructions state
+  let (l, r) = (M.!) (positions state) (currentPosition state)
+  let newPos = if instruction == L then l else r
+  MapState
+    { instructions = tail $ instructions state
+    , positions = positions state
+    , currentPosition = newPos
+    , steps = succ $ steps state
+    }
+
 day8part1 :: String -> String
-day8part1 _ = ""
+day8part1 = show . steps . head . dropWhile ((/= "ZZZ") . currentPosition) . iterate walk . startingState "AAA" . parseInput
 
 day8part2 :: String -> String
 day8part2 _ = ""
--- 2023/test/Day8Spec.hs
+++ 2023/test/Day8Spec.hs
@@ -6,13 +6,33 @@ import           Day8
 import           Test.Hspec
 
 input :: String
-input = ""
+input = unlines
+  [ "RL"
+  , ""
+  , "AAA = (BBB, CCC)"
+  , "BBB = (DDD, EEE)"
+  , "CCC = (ZZZ, GGG)"
+  , "DDD = (DDD, DDD)"
+  , "EEE = (EEE, EEE)"
+  , "GGG = (GGG, GGG)"
+  , "ZZZ = (ZZZ, ZZZ)"
+  ]
+
+input2 :: String
+input2 = unlines
+  [ "LLR"
+  , ""
+  , "AAA = (BBB, BBB)"
+  , "BBB = (AAA, ZZZ)"
+  , "ZZZ = (ZZZ, ZZZ)"
+  ]
 
 spec :: Spec
 spec = do
   describe "part 1" $ do
     it "runs provided examples" $ do
-      day8part1 input `shouldBe` ""
+      day8part1 input `shouldBe` "2"
+      day8part1 input2 `shouldBe` "6"
   describe "part 2" $ do
     it "runs provided examples" $ do
       day8part2 input `shouldBe` ""
